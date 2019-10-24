88350024953d7459996c0c140cf47f03ba4bf6d8
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-10-24 09:43:33 +0200

    Move login code to separate component

---
+++ src/components/TheLogin.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container page">
+    <div class="login">
+      <h1 class="title-1">Du er ikke logget inn</h1>
+      <p>Logg inn med BYR-konto (Google) for å bruke denne applikasjonen.</p>
+      <button class="btn" @click="login()">Logg inn</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      this.$login().then(() => {
+        location.reload();
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  min-height: calc(80vh - 5rem);
+}
+
+.btn {
+  margin-top: 2rem;
+}
+</style>
