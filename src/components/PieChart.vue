f8208087e58aa88fc5c6682836bba82cc2fe3887
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-10-29 10:05:05 +0100

    Use actual data for progress pie chart

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -17,14 +17,20 @@ export default {
     },
   },
 
+  computed: {
+    chosenQuarter() {
+      return this.$store.state.chosenQuarter;
+    },
+  },
+
   mounted() {
     this.pie = new Pie(this.$refs.svg);
-    this.pie.render(this.product);
+    this.pie.render(this.product, this.chosenQuarter);
   },
 
   watch: {
     product() {
-      this.pie.render(this.product);
+      this.pie.render(this.product, this.chosenQuarter);
     },
   },
 };
