a17f18cdb1df2c25f3fdb0d94dd241dc9f323d73
Aulon Mujaj <aulon.mujaj@gmail.com>
2020-07-27 11:11:25 +0200

    Use project_id and not app_id in url for mock

--- tests/e2e/support/signInAdminUser.js
+++ tests/e2e/support/signInAdminUser.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('signInAdminUser', () => {
   cy.get('input[type="password"]').type(Cypress.env('VUE_APP_TESTADMIN_PASSWORD'));
   cy.get('form').submit();
-  cy.wait(3000);
+  cy.wait(1000);
   cy.get('.usernav__display-name').contains('Test Admin');
 });
