2b685545372a88f63b8f64dc41e377ba980c2333
Jesper Kiledal <jesper.kiledal@gmail.com>
2020-08-21 10:41:17 +0100

    Add key result and objective rows

---
+++ src/components/KeyResultRow.vue
@@ -0,0 +1,45 @@
+<template>
+  <router-link :to="{ name: 'KeyResultHome', params: { keyResultId: keyResult.id } }" class="keyResult">
+    <span class="keyResult__name">{{ keyResult.name }}</span>
+
+    <ProgressBar class="keyResult__progression" :progression="keyResult.progression"></ProgressBar>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    keyResult: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  components: {
+    ProgressBar: () => import('@/components/ProgressBar.vue'),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.keyResult {
+  display: grid;
+  grid-template-columns: 1.5rem 1fr span(1, 0, span(6));
+  align-items: baseline;
+  padding: 0.5rem 0.75rem;
+  color: black;
+  text-decoration: none;
+}
+
+.keyResult__icon {
+  grid-column: 1;
+}
+
+.keyResult__name {
+  grid-column: 2;
+}
+
+.keyResult__progression {
+  grid-column: 3;
+}
+</style>
---
+++ src/components/ObjectiveRow.vue
@@ -0,0 +1,47 @@
+<template>
+  <router-link class="objective" to="#">
+    <span class="objective__icon fas fa-fw" :class="`fa-${objective.icon || 'trophy'}`"></span>
+    <span class="objective__name">{{ objective.name }}</span>
+
+    <ProgressBar class="objective__progression" :progression="objective.progression"></ProgressBar>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    objective: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  components: {
+    ProgressBar: () => import('@/components/ProgressBar.vue'),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.objective {
+  display: grid;
+  grid-template-columns: 1.5rem 1fr span(1, 0, span(6));
+  align-items: baseline;
+  padding: 0.5rem 0.75rem;
+  color: black;
+  text-decoration: none;
+}
+
+.objective__icon {
+  grid-column: 1;
+}
+
+.objective__name {
+  grid-column: 2;
+  font-weight: 500;
+}
+
+.objective__progression {
+  grid-column: 3;
+}
+</style>
