6174c5b6f580be8ac23e0dac3ebf88326f6191b8
Aulon Mujaj <aulon.mujaj@gmail.com>
2020-09-28 18:01:54 +0200

    Redesign/Keyresults: data is shown, but not much css is added

--- src/views/KeyResultHome.vue
+++ src/views/KeyResultHome.vue
@@ -3,25 +3,48 @@
     <div class="main">
       <h1 class="title-2">{{ activeKeyResult.name }}</h1>
 
-      <div class="keyres__value">
-        {{ activeKeyResult.currentValue }}
+      <div class="second">
+        <div class="second__value" style="display: flex; flex-direction: column;">
+          <span>
+            <i class="fas fa-chart-line"></i>
+            Verdi
+          </span>
+          <div>{{ activeKeyResult.currentValue }} {{ activeKeyResult.unit }}</div>
+
+          <button>Oppdater verdi</button>
+        </div>
+
+        <div class="second__graph">
+          <i class="fas fa-chart-line"></i>
+          Progresjon
+          <svg class="graph" ref="graph"></svg>
+        </div>
       </div>
 
-      <div class="keyres__graph">
-        <svg class="graph" ref="graph"></svg>
-      </div>
+      <h2 class="title-2">Historikk</h2>
 
-      <form @submit.prevent="addValue">
-        <input type="number" v-model.number="newValue" step="any" />
-        <button>Send</button>
-      </form>
-
-      <ul>
-        <li v-for="p in progress" :key="p.id">
-          <strong>Verdi</strong><span>{{ p.value }}</span> <strong>Dato</strong
-          ><span>{{ p.timestamp.toDate() }}</span>
-        </li>
-      </ul>
+      <table v-if="progress">
+        <thead>
+          <tr>
+            <th>Verdi</th>
+            <th>Dato</th>
+            <th>Registrert av</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="p in progress" :key="p.id">
+            <td>{{ p.value }}</td>
+            <td>{{ p.timestamp.toDate() }}</td>
+            <td>{{ p.createdBy.displayName || p.createdBy.id }}</td>
+            <td v-if="p.comment"><i class="fa fa-comment"></i></td>
+            <td>
+              <button>Slett</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 
     <widgets></widgets>
@@ -33,7 +56,6 @@ import { mapState } from 'vuex';
 import { db } from '@/config/firebaseConfig';
 import Progress from '@/db/Progress';
 import KeyResult from '@/db/KeyResult';
-import { fromUnixTime } from 'date-fns';
 import LineChart from '@/util/LineChart';
 
 export default {
@@ -79,16 +101,13 @@ export default {
     remove(id) {
       Progress.remove(this.activeKeyResult.id, id);
     },
-    fromUnixToDate(created) {
-      return fromUnixTime(created);
-    },
   },
 
   watch: {
     activeKeyResult: {
       immediate: true,
-      handler({ id }) {
-        this.$bind('progress', db.collection(`keyResults/${id}/progress`).orderBy('timestamp', 'desc'));
+      async handler({ id }) {
+        await this.$bind('progress', db.collection(`keyResults/${id}/progress`).orderBy('timestamp', 'desc'));
 
         if (this.graph) {
           this.graph.render(this.activeKeyResult, this.activePeriod, this.progress);
@@ -105,11 +124,32 @@ export default {
 .keyres {
   display: flex;
   flex-wrap: wrap;
+}
+
+.second {
+  display: flex;
+  width: 100%;
+  margin-bottom: 2rem;
 
   &__value {
+    align-self: flex-start;
+    width: span(2, 0, span(7));
     margin-bottom: 0.5rem;
+
+    padding: 1rem;
     background: white;
     border: 1px solid $color-grey-100;
+    box-shadow: 0 2px 3px rgba(black, 0.1);
+  }
+
+  &__graph {
+    width: span(5, 0, span(7));
+
+    margin-left: span(0, 1);
+    padding: 1rem;
+    background: white;
+    border: 1px solid $color-grey-100;
+    box-shadow: 0 2px 3px rgba(black, 0.1);
   }
 }
 
