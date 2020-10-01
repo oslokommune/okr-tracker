044b19fade4f22b6e0d9e38aa0e1add2b41efcbe
Aulon Mujaj <aulon.mujaj@gmail.com>
2020-10-01 12:00:13 +0200

    redesign/keyresulthome: temporary styles on keyResultDetails widget

--- src/components/widgets/WidgetKeyResultDetails.vue
+++ src/components/widgets/WidgetKeyResultDetails.vue
@@ -1,14 +1,33 @@
 <template>
   <Widget v-if="activeKeyResult" title="Detaljer" icon="info-circle">
-    Tilhører mål: {{ activeKeyResult.objective.name }} Periode: {{ activePeriod.name }}
-    {{ activePeriod.startDate.toDate() }} - {{ activePeriod.endDate.toDate() }} Antall oppdateringer:
-    {{ progress.length }}
+    <div style="display: flex; flex-direction: column;">
+      <h3 class="title-3">Tilhører mål</h3>
+      <span style="margin-bottom: 1rem;">
+        <i class="fa fa-trophy"></i>
+        {{ activeKeyResult.objective.name }}
+      </span>
+      <h3 class="title-3">Periode</h3>
+      <div style="display: flex; flex-direction: row; justify-content: space-between; margin-bottom: 1rem;">
+        <span>
+          <i class="fa fa-calendar-alt"></i>
+          {{ activePeriod.name }}
+        </span>
+        <span> {{ formatDate(activePeriod.startDate) }} - {{ formatDate(activePeriod.endDate) }} </span>
+      </div>
+      <h3 class="title-3">Antall oppdateringer</h3>
+      <span style="margin-bottom: 1rem;">
+        <i class="fa fa-chart-line"></i>
+        {{ progress.length }}
+      </span>
+    </div>
   </Widget>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 import { db } from '@/config/firebaseConfig';
+import { format } from 'date-fns';
+import { nb } from 'date-fns/locale';
 
 export default {
   name: 'WidgetKeyResultDetails',
@@ -32,6 +51,12 @@ export default {
         if (!keyresult) return;
         await this.$bind('progress', db.collection(`keyResults/${keyresult.id}/progress`));
       },
+    },
+  },
+
+  methods: {
+    formatDate(date) {
+      return format(date.toDate(), 'd MMMM Y', { locale: nb });
     },
   },
 };
