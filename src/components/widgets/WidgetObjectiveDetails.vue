487af7cde736c5a35094815d2cdda28dd439a8e1
Jesper Kiledal <jesper.kiledal@gmail.com>
2020-10-13 16:28:28 +0100

    Add widgets for objectiveHome

---
+++ src/components/widgets/WidgetObjectiveDetails.vue
@@ -0,0 +1,75 @@
+<template>
+  <Widget v-if="activeObjective" title="Detaljer" icon="info-circle">
+    <div class="details">
+      <h3 class="title-3">Periode</h3>
+
+      <div class="details__period" v-if="activeObjective.period && activeObjective.period.startDate">
+        <span>
+          <i class="fa fa-calendar-alt"></i>
+          {{ activeObjective.period.name }}
+        </span>
+        <span>
+          {{ formatDate(activeObjective.period.startDate) }} - {{ formatDate(activeObjective.period.endDate) }}
+        </span>
+      </div>
+
+      <h3 class="title-3">Progresjon</h3>
+      <span class="details--margin-bottom">
+        <i class="fa fa-chart-line"></i>
+        {{ activeObjective.progression }}
+      </span>
+
+      <h3 class="title-3">Vekt</h3>
+      <span class="details--margin-bottom">
+        <i class="fa fa-balance-scale"></i>
+        {{ activeObjective.weight }}
+      </span>
+    </div>
+  </Widget>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import { format } from 'date-fns';
+import { nb } from 'date-fns/locale';
+
+export default {
+  name: 'WidgetObjectiveDetails',
+
+  data: () => ({
+    progress: [],
+  }),
+
+  computed: {
+    ...mapState(['activeObjective']),
+  },
+
+  components: {
+    Widget: () => import('./Widget.vue'),
+  },
+
+  methods: {
+    formatDate(date) {
+      return format(date.toDate(), 'd MMMM Y', { locale: nb });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.details {
+  display: flex;
+  flex-direction: column;
+
+  &--margin-bottom {
+    margin-bottom: 1rem;
+  }
+
+  &__period {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
+}
+</style>
