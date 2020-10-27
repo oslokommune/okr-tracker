92dfc08473c5679aea4d51949c37dac5897ff216
Aulon Mujaj <aulon.mujaj@gmail.com>
2020-10-27 17:36:02 +0100

    redesign/toasts: removed last unsused function

--- src/util/toasts.js
+++ src/util/toasts.js
@@ -17,30 +17,6 @@ const close = (e, toastObject) => {
   toastObject.goAway(0);
 };
 
-export function deleteKeyRes() {
-  const options = {
-    action: [
-      {
-        text: i18n.tc('toaster.action.regret'),
-        onClick: (e, toastObject) => {
-          this.updateKeyRes(this.keyResObject).then(() => {
-            this.getAllData();
-            toastObject.goAway(0);
-          });
-        },
-      },
-      {
-        text: i18n.tc('btn.close'),
-        onClick: (e, toastObject) => {
-          toastObject.goAway(0);
-        },
-      },
-    ],
-  };
-
-  show(i18n.tc('toaster.delete.keyResult'), options);
-}
-
 export function error() {
   return showError(i18n.tc('toaster.error'));
 }
