62b8bf79c1d4133d0692417f7342820c169d3cca
Jesper Kiledal <jesper.kiledal@gmail.com>
2020-10-28 18:12:29 +0000

    Tidy up details widget

--- src/components/widgets/WidgetObjectiveDetails.vue
+++ src/components/widgets/WidgetObjectiveDetails.vue
@@ -1,37 +1,80 @@
 <template>
   <Widget :widget-id="widgetId" v-if="activeObjective" :title="$t('general.details')" icon="info-circle">
     <div class="details">
-      <h3 class="title-3">{{ $t('objective.period') }}</h3>
-
-      <div class="details__period" v-if="activeObjective.period && activeObjective.period.startDate">
-        <span>
-          <i class="fa fa-calendar-alt"></i>
-          {{ activeObjective.period.name }}
-        </span>
-        <span>
-          {{ formatDate(activeObjective.period.startDate) }} - {{ formatDate(activeObjective.period.endDate) }}
-        </span>
+      <div class="details__item" v-if="activeObjective.period && activeObjective.period.startDate">
+        <h3 class="title-3 details__item-heading">{{ $t('objective.period') }}</h3>
+        <div class="details__item-body">
+          <div class="details__item-icon fa fa-calendar-alt"></div>
+          <div class="details__item-value">
+            {{ activeObjective.period.name }} ({{ formatPeriodDates(activeObjective.period) }})
+          </div>
+        </div>
       </div>
 
-      <h3 class="title-3">{{ $t('objective.progression') }}</h3>
-      <span class="details--margin-bottom">
-        <i class="fa fa-chart-line"></i>
-        {{ activeObjective.progression }}
-      </span>
+      <div class="details__item" v-if="activeObjective.created">
+        <h3 class="title-3 details__item-heading">{{ $t('objective.created') }}</h3>
+        <div class="details__item-body">
+          <div class="details__item-icon fa fa-calendar"></div>
+          <div class="details__item-value">{{ formatDate(activeObjective.created) }}</div>
+        </div>
+      </div>
 
-      <h3 class="title-3">{{ $t('objective.weight') }}</h3>
-      <span class="details--margin-bottom">
-        <i class="fa fa-balance-scale"></i>
-        {{ activeObjective.weight }}
-      </span>
+      <div class="details__item" v-if="activeObjective.createdBy">
+        <h3 class="title-3 details__item-heading">{{ $t('objective.createdBy') }}</h3>
+
+        <div class="details__item-body">
+          <div class="details__item-icon fa fa-user"></div>
+          <div class="details__item-value">
+            <router-link
+              v-if="activeObjective.createdBy.id"
+              :to="{ name: 'User', params: { id: activeObjective.createdBy.id } }"
+            >
+              {{ activeObjective.createdBy.displayName || activeObjective.createdBy.id }}
+            </router-link>
+            <span v-else>{{ activeObjective.createdBy }}</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="details__item" v-if="activeObjective.edited">
+        <h3 class="title-3 details__item-heading">{{ $t('objective.edited') }}</h3>
+        <div class="details__item-body">
+          <div class="details__item-icon fa fa-calendar"></div>
+          <div class="details__item-value">{{ formatDate(activeObjective.edited) }}</div>
+        </div>
+      </div>
+
+      <div class="details__item" v-if="activeObjective.editedBy">
+        <h3 class="title-3 details__item-heading">{{ $t('objective.editedBy') }}</h3>
+
+        <div class="details__item-body">
+          <div class="details__item-icon fa fa-user"></div>
+          <div class="details__item-value">
+            <router-link
+              v-if="activeObjective.editedBy.id"
+              :to="{ name: 'User', params: { id: activeObjective.editedBy.id } }"
+            >
+              {{ activeObjective.editedBy.displayName || activeObjective.editedBy.id }}
+            </router-link>
+            <span v-else>{{ activeObjective.editedBy }}</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="details__item">
+        <h3 class="title-3 details__item-heading">Heading</h3>
+        <div class="details__item-body">
+          <div class="details__item-icon fa fa-user"></div>
+          <div class="details__item-value">User</div>
+        </div>
+      </div>
     </div>
   </Widget>
 </template>
 
 <script>
 import { mapState } from 'vuex';
-import { format } from 'date-fns';
-import { nb } from 'date-fns/locale';
+import { periodDates, dateShort, dateLong } from '@/util/formatDate';
 
 export default {
   name: 'WidgetObjectiveDetails',
@@ -56,27 +99,12 @@ export default {
   },
 
   methods: {
+    formatPeriodDates(period) {
+      return periodDates(period, dateShort);
+    },
     formatDate(date) {
-      return format(date.toDate(), 'd MMMM Y', { locale: nb });
+      return dateLong(date.toDate());
     },
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.details {
-  display: flex;
-  flex-direction: column;
-
-  &--margin-bottom {
-    margin-bottom: 1rem;
-  }
-
-  &__period {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    margin-bottom: 1rem;
-  }
-}
-</style>
