082782993d560dfe2859bdaaaa0055fe0f518360
Jesper Kiledal <jesper.kiledal@gmail.com>
2020-10-29 16:17:01 +0000

    Fix bug in breadcrumbs

--- src/store/actions/set_active_key_result.js
+++ src/store/actions/set_active_key_result.js
@@ -1,3 +1,4 @@
+import store from '@/store'; // eslint-disable-line
 import { firestoreAction } from 'vuexfire';
 import { db } from '@/config/firebaseConfig';
 
@@ -6,6 +7,12 @@ export default firestoreAction(async ({ bindFirestoreRef, unbindFirestoreRef }, 
     return unbindFirestoreRef('activeKeyResult');
   }
   const reference = db.collection('keyResults').doc(id);
+
+  const { objective } = await reference.get().then(snapshot => snapshot.data());
+
+  if (!store.state.activeObjective || store.state.activeObjective.id !== objective.id) {
+    await bindFirestoreRef('activeObjective', objective, { maxRefDepth: 1 });
+  }
 
   return bindFirestoreRef('activeKeyResult', reference, { maxRefDepth: 1 });
 });
