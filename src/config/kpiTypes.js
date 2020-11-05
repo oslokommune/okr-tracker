b3481739fe0281460a671e90d8dffe028e74f922
Jesper Kiledal <jesper.kiledal@gmail.com>
2020-11-05 14:01:39 +0000

    Force order of KPIs + include non-existent KPIs on ItemHome

---
+++ src/config/kpiTypes.js
@@ -0,0 +1,20 @@
+import { format } from 'd3';
+import i18n from '@/locale/i18n';
+
+export default {
+  users: {
+    label: i18n.t('kpi.types.users'),
+    icon: 'fa-user',
+    formatValue: format('.2s'),
+  },
+  satisfaction: {
+    label: i18n.t('kpi.types.satisfaction'),
+    icon: 'fa-smile',
+    formatValue: format('.2p'),
+  },
+  conversion: {
+    label: i18n.t('kpi.types.conversion'),
+    icon: 'fa-check-square',
+    formatValue: format('.2p'),
+  },
+};
