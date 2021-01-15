315d8e54d20aed0aa7a65c0aa45c4e64d378dbc1
Aulon Mujaj <aulon.mujaj@gmail.com>
2021-01-15 15:39:47 +0100

    feature/search: added search bars in adminItems for products/organizations/departments

--- src/views/Admin/components/AdminItems.vue
+++ src/views/Admin/components/AdminItems.vue
@@ -5,8 +5,18 @@
         <h2 class="title-2">{{ $t('general.organizations') }}</h2>
 
         <div class="col">
+          <div v-if="filteredOrgs.length > 10" class="search">
+            <label class="form__group">
+              <input
+                v-model="queryOrgs"
+                class="form__field"
+                type="text"
+                :placeholder="$t('admin.organization.search', { count: organizations.length })"
+              />
+            </label>
+          </div>
           <div class="col__body">
-            <div v-for="organization in organizations" :key="organization.id" class="col__row">
+            <div v-for="organization in filteredOrgs" :key="organization.id" class="col__row">
               <router-link class="col__link" :to="{ name: 'ItemAdmin', params: { slug: organization.slug } }">
                 <i class="col__icon fa fa-industry" />
                 {{ organization.name }}
@@ -25,8 +35,18 @@
       <div>
         <h2 class="title-2">{{ $t('general.departments') }}</h2>
         <div class="col">
+          <div v-if="filteredDeps.length > 15" class="search">
+            <label class="form__group">
+              <input
+                v-model="queryDeps"
+                class="form__field"
+                type="text"
+                :placeholder="$t('admin.department.search', { count: departments.length })"
+              />
+            </label>
+          </div>
           <div class="col__body">
-            <div v-for="department in departments" :key="department.id" class="col__row">
+            <div v-for="department in filteredDeps" :key="department.id" class="col__row">
               <router-link class="col__link" :to="{ name: 'ItemAdmin', params: { slug: department.slug } }">
                 <i class="col__icon fa fa-cubes" />
                 {{ department.name }}
@@ -45,8 +65,18 @@
       <div>
         <h2 class="title-2">{{ $t('general.products') }}</h2>
         <div class="col">
+          <div v-if="filteredProds.length > 15" class="search">
+            <label class="form__group">
+              <input
+                v-model="queryProds"
+                class="form__field"
+                type="text"
+                :placeholder="$t('admin.product.search', { count: products.length })"
+              />
+            </label>
+          </div>
           <div class="col__body">
-            <div v-for="product in products" :key="product.id" class="col__row">
+            <div v-for="product in filteredProds" :key="product.id" class="col__row">
               <router-link class="col__link" :to="{ name: 'ItemAdmin', params: { slug: product.slug } }">
                 <i class="col__icon fa fa-cube" />
                 {{ product.name }}
@@ -73,7 +103,22 @@
 </template>
 
 <script>
+import Fuse from 'fuse.js';
 import { db } from '@/config/firebaseConfig';
+
+const fuseSettings = {
+  threshold: 0.5,
+  keys: [
+    {
+      name: 'slug',
+      weight: 0.25,
+    },
+    {
+      name: 'name',
+      weight: 0.5,
+    },
+  ],
+};
 
 export default {
   name: 'AdminItems',
@@ -83,6 +128,15 @@ export default {
     organizations: [],
     departments: [],
     products: [],
+    fuseOrgs: null,
+    fuseDeps: null,
+    fuseProds: null,
+    queryOrgs: '',
+    queryDeps: '',
+    queryProds: '',
+    filteredOrgs: [],
+    filteredDeps: [],
+    filteredProds: [],
   }),
 
   watch: {
@@ -91,17 +145,65 @@ export default {
       handler() {
         this.$bind(
           'organizations',
-          db.collection('organizations').where('archived', 'in', [false, this.showArchived]).orderBy('name')
+          db.collection('organizations').where('archived', 'in', [false, this.showArchived]).orderBy('slug')
         );
         this.$bind(
           'departments',
-          db.collection('departments').where('archived', 'in', [false, this.showArchived]).orderBy('name')
+          db.collection('departments').where('archived', 'in', [false, this.showArchived]).orderBy('slug')
         );
         this.$bind(
           'products',
-          db.collection('products').where('archived', 'in', [false, this.showArchived]).orderBy('name')
+          db.collection('products').where('archived', 'in', [false, this.showArchived]).orderBy('slug')
         );
       },
+    },
+
+    organizations: {
+      immediate: true,
+      handler() {
+        this.filteredOrgs = this.organizations;
+        this.fuseOrgs = new Fuse(this.filteredOrgs, fuseSettings);
+      },
+    },
+
+    departments: {
+      immediate: true,
+      handler() {
+        this.filteredDeps = this.departments;
+        this.fuseDeps = new Fuse(this.filteredDeps, fuseSettings);
+      },
+    },
+
+    products: {
+      immediate: true,
+      handler() {
+        this.filteredProds = this.products;
+        this.fuseProds = new Fuse(this.filteredProds, fuseSettings);
+      },
+    },
+
+    queryOrgs(str) {
+      if (str.length < 1) {
+        this.filteredOrgs = this.organizations;
+      } else {
+        this.filteredOrgs = this.fuseOrgs.search(str).map(({ item }) => item);
+      }
+    },
+
+    queryDeps(str) {
+      if (str.length < 1) {
+        this.filteredDeps = this.departments;
+      } else {
+        this.filteredDeps = this.fuseDeps.search(str).map(({ item }) => item);
+      }
+    },
+
+    queryProds(str) {
+      if (str.length < 1) {
+        this.filteredProds = this.products;
+      } else {
+        this.filteredProds = this.fuseProds.search(str).map(({ item }) => item);
+      }
     },
   },
 };
