bc5e20093fc97a02db0820105cd2f1380cd199d5
Aulon Mujaj <aulon.mujaj@gmail.com>
2021-04-28 17:25:21 +0200

    feat(redesign/itemHome): first draft of itemHome - one div overlaying another

--- src/views/ItemHome.vue
+++ src/views/ItemHome.vue
@@ -3,31 +3,35 @@
     <div class="main">
       <kpis v-if="kpis.length" :kpis="kpis"></kpis>
 
-      <h2 class="title-2">{{ $t('general.OKRsLong') }}</h2>
-      <period-selector />
-      <action-bar v-if="tree.length" />
+      <div class="itemHome">
+        <div class="itemHome--header">
+          <h2 class="title-2">{{ $t('general.OKRsLong') }}</h2>
+          <period-selector />
+          <action-bar v-if="tree.length" />
+        </div>
 
-      <empty-state
-        v-if="!tree.length"
-        :icon="'exclamation'"
-        :heading="$t('empty.noPeriods.heading')"
-        :body="$t('empty.noPeriods.body')"
-      >
-        <router-link v-if="hasEditRights" class="btn btn--ter" :to="{ name: 'ItemAdminOKRs' }">
-          {{ $t('empty.noPeriods.buttonText') }}
-        </router-link>
-      </empty-state>
+        <empty-state
+          v-if="!tree.length"
+          :icon="'exclamation'"
+          :heading="$t('empty.noPeriods.heading')"
+          :body="$t('empty.noPeriods.body')"
+        >
+          <router-link v-if="hasEditRights" class="btn btn--ter" :to="{ name: 'ItemAdminOKRs' }">
+            {{ $t('empty.noPeriods.buttonText') }}
+          </router-link>
+        </empty-state>
 
-      <ul v-if="tree">
-        <li v-for="objective in tree" :key="objective.id">
-          <objective-row :objective="objective"></objective-row>
-          <ul v-if="objective.keyResults" class="group">
-            <li v-for="keyResult in objective.keyResults" :key="keyResult.id" class="keyResultRow">
-              <key-result-row :key-result="keyResult"></key-result-row>
-            </li>
-          </ul>
-        </li>
-      </ul>
+        <ul v-if="tree" class="itemHome--tree">
+          <li v-for="objective in tree" :key="objective.id" class="itemHome--tree--item">
+            <objective-row :objective="objective"></objective-row>
+            <ul v-if="objective.keyResults" class="group">
+              <li v-for="keyResult in objective.keyResults" :key="keyResult.id" class="keyResultRow">
+                <key-result-row :key-result="keyResult"></key-result-row>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
 
     <widgets></widgets>
@@ -82,5 +86,31 @@ export default {
   &:first-child {
     border-top: 0;
   }
+}
+
+.itemHome {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  grid-column-gap: 0;
+  grid-row-gap: 0;
+}
+
+.itemHome--header {
+  grid-area: 1 / 1 / 3 / 2;
+  background: white;
+  padding: 1rem;
+  margin-bottom: 8rem;
+}
+
+.itemHome--tree {
+  grid-area: 2 / 1 / 4 / 2;
+}
+
+.itemHome--tree--item {
+  background: white;
+  margin: 0 1rem 1rem 1rem;
+  padding: 1rem;
+  box-shadow: -2px 1px 5px 2px rgb(147 147 153 / 30%)
 }
 </style>
