ded106cf495cbc3decd5d3fb5ce70de75152a79b
Aulon Mujaj <aulon.mujaj@gmail.com>
2021-08-04 09:48:11 +0200

    rewritten audit endpoint from 3 to 1 and added queryparams instead

--- functions/api/routes/audit.js
+++ functions/api/routes/audit.js
@@ -5,36 +5,43 @@ const parse = require('date-fns/parseISO');
 
 const db = admin.firestore();
 
-const collection = db.collection('audit');
-
-const validate = [query('fromDate').trim().escape(), query('toDate').trim().escape()];
-const validateEvent = [param('type').trim().escape()];
-const validateDoc = [param('documentType').trim().escape(), param('id').trim().escape()];
+const validate = [
+  query('fromDate').trim().escape(),
+  query('toDate').trim().escape(),
+  query('documentType').trim().escape(),
+  query('documentId').trim().escape(),
+  query('eventType').trim().escape(),
+];
 
 router.get('/', ...validate, async (req, res) => {
   const sanitized = matchedData(req);
-  const { fromDate, toDate } = sanitized;
-
-  let from;
-  let to;
-  if (fromDate) {
-    from = admin.firestore.Timestamp.fromDate(parse(fromDate));
-  }
-  if (toDate) {
-    to = admin.firestore.Timestamp.fromDate(parse(toDate));
-  }
+  const { fromDate, toDate, documentType, documentId, eventType } = sanitized;
 
   try {
-    let snapshot;
-    if (fromDate && !toDate) {
-      snapshot = await collection.where('timestamp', '>=', from).get();
-    } else if (!fromDate && toDate) {
-      snapshot = await collection.where('timestamp', '<=', to).get();
-    } else if (fromDate && toDate) {
-      snapshot = await collection.where('timestamp', '>=', from).where('timestamp', '<=', to).get();
-    } else {
-      snapshot = await collection.get();
+    let q = db.collection('audit');
+
+    const from = fromDate ? admin.firestore.Timestamp.fromDate(parse(fromDate)) : undefined;
+    const to = toDate ? admin.firestore.Timestamp.fromDate(parse(toDate)) : undefined;
+
+    if (from) {
+      q = q.where('timestamp', '>=', from);
     }
+    if (to) {
+      q = q.where('timestamp', '<=', to);
+    }
+    if (documentType || documentId) {
+      if (!documentType || !documentId) {
+        res.status(404).send('DocumentType and documentId must be set');
+        return;
+      }
+      const documentRef = await db.collection(documentType).doc(documentId).get();
+      q = q.where('documentRef', '==', documentRef.ref);
+    }
+    if (eventType) {
+      q = q.where('event', '==', eventType);
+    }
+
+    const snapshot = await q.get();
 
     if (snapshot.size === 0) {
       res.status(404).send('Audit log is empty');
@@ -52,87 +59,6 @@ router.get('/', ...validate, async (req, res) => {
         return {
           ...data,
           document: doc,
-          user: user.email ? user.email : 'system',
-          timestamp: data.timestamp.toDate(),
-        };
-      })
-    );
-
-    res.json({
-      data: output,
-    });
-  } catch (e) {
-    console.error('ERROR: ', e.message);
-    res.status(500).send('Cannot get audit log');
-  }
-});
-
-router.get('/event/:type', ...validateEvent, async (req, res) => {
-  const sanitized = matchedData(req);
-  const { type } = sanitized;
-
-  try {
-    const snapshot = await collection.where('event', '==', type).get();
-
-    console.log(snapshot.size);
-    if (snapshot.size === 0) {
-      res.status(404).send('Audit log is empty');
-      return;
-    }
-
-    const output = await Promise.all(
-      snapshot.docs.map(async (d) => {
-        const data = d.data();
-        const doc = await data.documentRef.get().then((snap) => snap.data());
-        let user = 'system';
-        if (data.user !== 'system') {
-          user = await data.user.get().then((snap) => snap.data());
-        }
-        return {
-          ...data,
-          document: doc,
-          user: user.email ? user.email : 'system',
-          timestamp: data.timestamp.toDate(),
-        };
-      })
-    );
-
-    res.json({
-      data: output,
-    });
-  } catch (e) {
-    console.error('ERROR: ', e.message);
-    res.status(500).send('Cannot get audit log');
-  }
-});
-
-router.get('/document/:documentType/:id', ...validateDoc, async (req, res) => {
-  const sanitized = matchedData(req);
-  const { documentType, id } = sanitized;
-
-  try {
-    console.log(documentType, id);
-    const documentRef = await db.collection(documentType).doc(id).get();
-
-    const snapshot = await collection.where('documentRef', '==', documentRef.ref).get();
-
-    console.log(snapshot);
-    if (snapshot.size === 0) {
-      res.status(404).send('Audit log is empty');
-      return;
-    }
-
-    const output = await Promise.all(
-      snapshot.docs.map(async (d) => {
-        const data = d.data();
-        const doc = await data.documentRef.get().then((snap) => snap.data());
-        let user = 'system';
-        if (data.user !== 'system') {
-          user = await data.user.get().then((snap) => snap.data());
-        }
-        return {
-          ...data,
-          documentRef: doc,
           user: user.email ? user.email : 'system',
           timestamp: data.timestamp.toDate(),
         };
