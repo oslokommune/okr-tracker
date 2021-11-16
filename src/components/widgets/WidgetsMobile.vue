49d0be81673d4b462a590139de2125a991917ffd
Aulon Mujaj <aulon.mujaj@gmail.com>
2021-11-16 23:39:11 +0100

    feat(redesign): QoL changes to the widgetsMobile.vue

--- src/components/widgets/WidgetsMobile.vue
+++ src/components/widgets/WidgetsMobile.vue
@@ -1,7 +1,11 @@
 <template>
   <aside v-if="activeItem">
     <div class="widgets">
-      <widget-progression widget-id="itemHome.progression" type="period" />
+      <widget-progression
+        widget-id="itemHome.progression"
+        :type="$route.name === 'ItemHome' ? 'period' : 'objective'"
+        :dimmed="$route.name !=='ItemHome'"
+      />
       <widget-mission-statement widget-id="itemHome.missionsStatement" />
       <widget-team widget-id="itemHome.team" />
       <widget-child-items widget-id="itemHome.children" />
