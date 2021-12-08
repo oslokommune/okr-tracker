51a0912c4eca5baeca8455db97566c254513eb2a
Aulon Mujaj <aulon.mujaj@gmail.com>
2021-12-08 16:05:07 +0100

    feat(modal-for-user-details): fixed som small bugs

--- src/components/ProfileModal.vue
+++ src/components/ProfileModal.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="profilemodal__overlay " @click.self="close">
+  <div v-if="user" class="profileModal__overlay " @click.self="close">
 
-    <div class="modal__main--flex" :class="[ me? 'profilemodal__upper-right' : 'profilemodal__centered']">
+    <div class="modal__main--flex" :class="me ? 'profileModal__upper-right' : 'profileModal__centered'">
       <div v-if="me" class="closeBtn" >
         <a href="#" @click="close"><i class="icon fa fa-white fa-fw fa-times-circle"/></a>
       </div>
 
       <div class="column">
-        <div class="profilemodal__heading">{{ $t('user.profile')}}</div>
+        <div class="profileModal__heading">{{ $t('user.profile')}}</div>
         <validation-observer v-slot="{ handleSubmit }">
           <form id="updateUser" @submit.prevent="handleSubmit(save)">
-            <span class="profilemodal__label">{{ $t('fields.name') }}</span>
+            <span class="profileModal__label">{{ $t('fields.name') }}</span>
             <input
               v-model="thisUser.displayName"
               rules="required"
               :disabled="!me"
               @input="edit"
-              class="profilemodal__input"
+              class="profileModal__input"
             />
           </form>
         </validation-observer>
         <label class="form-group">
-          <span class="profilemodal__label">{{ $t('user.position.title') }}</span>
+          <span class="profileModal__label">{{ $t('user.position.title') }}</span>
           <v-select
             v-if="me"
             v-model="thisUser.position"
@@ -32,14 +32,14 @@
             @input="edit"
           >
           </v-select>
-          <div v-else class="profilemodal__input">
+          <div v-else class="profileModal__input">
             <span>
               {{ thisUser && thisUser.position? thisUser.position : $t('user.position.member') }}
             </span>
           </div>
         </label>
         <label v-if="me" class="form-group">
-          <span class="profilemodal__label">{{ $t('user.selectLanguage') }}</span>
+          <span class="profileModal__label">{{ $t('user.selectLanguage') }}</span>
           <v-select
             v-model="thisUser.preferences.lang"
             :options="languages"
@@ -48,12 +48,12 @@
           >
           </v-select>
         </label>
-        <div v-if="me" class="profilemodal__save-button">
+        <div v-if="me" class="profileModal__save-button">
           <button class="btn btn--sec" form="updateUser" :disabled="loading || !changes">{{ $t('btn.save') }}</button>
         </div>
       </div>
       <div class="column">
-        <div v-if="me" class="profilemodal__heading">
+        <div v-if="me" class="profileModal__heading">
           {{ $t('user.access') }}
         </div>
         <template v-if="me && user.superAdmin">
@@ -70,19 +70,19 @@
           </div>
         </template>
 
-        <div class="profilemodal__heading">
+        <div class="profileModal__heading">
           {{ me? $t('user.myProducts') : $t('user.products')}}
         </div>
 
         <div>
           <ul>
             <li v-for="product in products" :key="product.id">
-              <div class="profilemodal__label">{{ product.department.name }}</div>
+              <div class="profileModal__label">{{ product.department.name }}</div>
               <div class="product">{{ product.name }}</div>
             </li>
           </ul>
         </div>
-        <div v-if="me" class="profilemodal__heading">
+        <div v-if="me" class="profileModal__heading">
           {{ $t('general.admin') }}
         </div>
         <div v-if="me" class="sidebar__group sidebar__bottom button-col">
@@ -102,29 +102,29 @@
   </div>
 </template>
 <script>
-  import { db } from '@/config/firebaseConfig';
-  import { auth } from '@/config/firebaseConfig';
-  import ThemeToggle from '@/components/ThemeToggle.vue';
-  import User from '@/db/User';
-  import { jobPositions } from '@/config/jobPositions';
-  import { mapActions } from "vuex";
+import { mapActions } from "vuex";
+import { db, auth } from '@/config/firebaseConfig';
+import ThemeToggle from '@/components/ThemeToggle.vue';
+import User from '@/db/User';
+import { jobPositions } from '@/config/jobPositions';
 
   export default {
-    name: 'ProfileModal',
+    name: 'profileModal',
 
     components: {
       ThemeToggle,
-      User,
-      jobPositions,
     },
 
     props: {
       myProfile: {
         type: Boolean,
+        required: false,
         default: false
       },
+
       id: {
         type: String,
+        required: true,
       }
     },
 
@@ -149,13 +149,13 @@
     watch: {
       id: {
         immediate: true,
-        handler(id) {
-          const userRef = db.doc(`users/${id}`);
-          const productsRef = db
+        async handler(id) {
+          const userRef = await db.doc(`users/${id}`);
+          const productsRef = await db
             .collection('products')
             .where('team', 'array-contains', userRef)
             .where('archived', '==', false);
-          const auditRef = db.collection('audit').where('user', '==', userRef).orderBy('timestamp', 'desc').limit(10);
+          const auditRef = await db.collection('audit').where('user', '==', userRef).orderBy('timestamp', 'desc').limit(10);
 
           this.$bind('user', userRef);
           this.$bind('products', productsRef, { maxRefDepth: 1 });
@@ -208,7 +208,7 @@
 <style lang="scss" scoped>
   @use '@/styles/typography';
 
-  .profilemodal__upper-right {
+  .profileModal__upper-right {
     position: fixed;
     top: 4.5rem;
     right: 0.5rem;
@@ -216,23 +216,23 @@
 
   .fa-white {color: white;}
 
-  .profilemodal__centered {
+  .profileModal__centered {
     position: fixed;
     horiz-align: center;
     vertical-align: center;
   }
 
   .closeBtn {
-    display: block;
-    float:right;
     position:fixed;
     top:1rem;
     right: 1.5rem;
     z-index: 1002;
+    display: block;
+    float:right;
     font-size: 1.5rem;
   }
 
-  .profilemodal__overlay {
+  .profileModal__overlay {
     position: fixed;
     top: 0;
     right: 0;
@@ -248,9 +248,9 @@
     background: rgba(black, 0.5);
   }
 
-  .profilemodal__heading {
-    padding-bottom: 1rem;
+  .profileModal__heading {
     padding-top: 1rem;
+    padding-bottom: 1rem;
     font-weight: 500;
     text-transform: uppercase;
   }
@@ -260,7 +260,7 @@
     padding-bottom: 1rem;
   }
 
-  .profilemodal__label {
+  .profileModal__label {
     display: inline-block;
     margin-top: 0.8rem;
     margin-bottom: 0.5rem;
@@ -270,22 +270,22 @@
     letter-spacing: -0.03rem;
   }
 
-  .profilemodal__input {
+  .profileModal__input {
     width: 100%;
+    padding: 0.7rem;
     background: var(--color-grey-50);
     border-radius: 0;
-    padding: 0.7rem;
   }
 
   .modal__link {
-    text-decoration: none;
-    background: none;
-    border: none;
     padding-top: 0.2rem;
     padding-bottom: 0.2rem;
     padding-left: 0rem;
-    cursor: pointer;
     text-align: left;
+    text-decoration: none;
+    background: none;
+    border: none;
+    cursor: pointer;
     &:hover {
       font-weight: bold;
     }
@@ -294,14 +294,14 @@
   .modal__main--flex {
     z-index: 100;
     display: flex;
-    flex-direction: column;
     flex-basis: 100%;
+    flex-direction: column;
+    max-height: calc(100vh - 10px);
+    overflow-y: auto;
     color: var(--color-text);
     background: white;
     border-radius: 1px;
     box-shadow: 0 0.25rem 0.45rem rgba(black, 0.5);
-    max-height: calc(100vh - 10px);
-    overflow-y: auto;
 
     @media screen and (min-width: bp(s)){
       flex-direction: row;
@@ -314,7 +314,7 @@
     padding: 3rem;
   }
 
-  .profilemodal__save-button {
+  .profileModal__save-button {
     display: flex;
     flex-direction: row;
     justify-content: flex-end;
