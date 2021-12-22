f722d1b66a71ab25c4d6f09b9adcdd8e68b4f181
Anne M. P <annemerete.pettersen@origo.oslo.kommune.no>
2021-12-22 10:36:12 +0100

    Text update for key result progress

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,6 @@
+import { format } from 'd3-format';
+import vueI18n from '@/locale/i18n';
+
 export { default as api } from './axios';
 export { dateLong } from './format';
 export { dateShort } from './format';
@@ -11,3 +14,25 @@ export { default as findSlugAndRedirect } from './findSlugAndRedirect';
 export { default as tableOfContent } from './tableOfContent';
 export { default as toastArchiveAndRevert } from './toastUtils';
 export { default as validateEmail } from './validateEmail';
+
+export const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+export const progressText = (progress, unit) => {return vueI18n.t('progress.remaining', { progress: format('.1~f')(progress), unit: unit })}
+
+export const remainingKeyResultProgress = (keyResult) => {
+  if (keyResult.currentValue >= keyResult.targetValue) {
+    const maxMessages = Object.keys(vueI18n.t('progress.completedMessages')).length;
+    return vueI18n.t(`progress.completedMessages.${randomIntFromInterval(1, maxMessages)}`);
+  }
+
+  if (keyResult.targetValue < keyResult.startValue) {
+    if (!keyResult.currentValue) {
+      return progressText(keyResult.startValue, keyResult.unit)
+    }
+    return progressText(keyResult.startValue - keyResult.currentValue, keyResult.unit)
+  }
+  if (!keyResult.currentValue) {
+    return progressText(keyResult.targetValue, keyResult.unit)
+  }
+  return progressText(keyResult.targetValue - keyResult.currentValue, keyResult.unit)
+};
