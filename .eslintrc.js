70e376cd9c14eca0973ce9048f84c63645b9b3c7
maynkj <maynkj@protonmail.com>
2022-03-04 10:16:14 +0100

    Update config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-use-before-define': 'off',
     'func-names': 'off',
     'no-restricted-syntax': 'off',
+    'import/extensions': ['always'],
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
