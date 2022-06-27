3bf941518dff757db1e0bb6c477c3a1170a14dde
maynkj <maynkj@protonmail.com>
2022-06-27 15:07:49 +0200

    Add DashboardResultIndicatorsSection component.

---
+++ src/components/DashboardResultIndicatorsSection.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="dashboardResultIndicatorsSection">
+    <div class="dashboardResultIndicatorsSection__header">
+      <h3 class="title-3">{{ resultIndicator.name }}</h3>
+      <v-select
+        label="name"
+        :options="resultIndicatorPeriods"
+        :value="currentResultIndicatorPeriod"
+        :components="selectComponents"
+        :searchable="false"
+        @input="setCurrentResultIndicatorPeriod"
+      />
+    </div>
+    <svg ref="progressGraphSvg" />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import { extent } from 'd3-array';
+import { db } from '@/config/firebaseConfig';
+import LineChart from '@/util/LineChart';
+import IconChevronThinDown from './IconChevronThinDown.vue';
+
+const getResultIndicatorPeriods = () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  return {
+    quarter: {
+      name: '1. kvartal',
+      value: 'quarter',
+      startDate: new Date(currentYear, 0, 1),
+      endDate: new Date(currentYear, 2, 31),
+    },
+    tertial: {
+      name: 'tertial',
+      value: 'tertial',
+      startDate: new Date(currentYear, 0, 1),
+      endDate: new Date(currentYear, 3, 30),
+    },
+    year: {
+      name: 'hittil i år',
+      value: 'year',
+      startDate: new Date(currentYear, 0, 1),
+      endDate: currentDate,
+    },
+  };
+};
+
+const RESULT_INDICATOR_PERIODS = getResultIndicatorPeriods();
+
+export default {
+  name: 'DashboardResultIndicatorsSection',
+
+  data: () => ({
+    resultIndicator: null,
+    progressCollection: [],
+    isPOCDepartment: false,
+    resultIndicatorPeriods: Object.values(RESULT_INDICATOR_PERIODS).map(
+      (period) => period
+    ),
+    currentResultIndicatorPeriod: RESULT_INDICATOR_PERIODS.quarter,
+    selectComponents: { Deselect: null, OpenIndicator: IconChevronThinDown },
+  }),
+
+  computed: {
+    ...mapState(['kpis', 'theme']),
+  },
+
+  watch: {
+    $route: {
+      immediate: true,
+      async handler(current) {
+        const { resultIndicatorPeriod } = current.query;
+
+        if (resultIndicatorPeriod) {
+          this.currentResultIndicatorPeriod =
+            RESULT_INDICATOR_PERIODS[resultIndicatorPeriod];
+        }
+      },
+    },
+    currentResultIndicatorPeriod() {
+      this.getProgressData(this.resultIndicator.id);
+    },
+    kpis: {
+      immediate: true,
+      async handler([resultIndicator]) {
+        if (resultIndicator) {
+          this.resultIndicator = resultIndicator;
+        }
+      },
+    },
+    resultIndicator: {
+      immediate: true,
+      handler(resultIndicator) {
+        if (resultIndicator) {
+          this.getProgressData();
+        }
+      },
+    },
+    progressCollection() {
+      this.renderGraph();
+    },
+  },
+
+  methods: {
+    setActiveTab(tabIndex) {
+      this.activeTab = tabIndex;
+    },
+    async getProgressData() {
+      const collection = db
+        .collection(`kpis/${this.resultIndicator.id}/progress`)
+        .where('timestamp', '>', this.currentResultIndicatorPeriod.startDate)
+        .where('timestamp', '<', this.currentResultIndicatorPeriod.endDate)
+        .orderBy('timestamp', 'desc');
+
+      await this.$bind('progressCollection', collection);
+    },
+    setCurrentResultIndicatorPeriod(selectedPeriod) {
+      this.currentResultIndicatorPeriod = selectedPeriod;
+      this.$router.push({
+        query: { resultIndicatorPeriod: selectedPeriod.value },
+      });
+    },
+    renderGraph() {
+      if (!this.graph) {
+        this.graph = new LineChart(this.$refs.progressGraphSvg, {
+          colorMode: this.theme,
+        });
+      }
+
+      const [startValue, targetValue] = extent(
+        this.progressCollection.map(({ value }) => value)
+      );
+      const [startDate, endDate] = extent(
+        this.progressCollection.map(({ timestamp }) => timestamp)
+      );
+
+      this.graph.render({
+        obj: {
+          startValue,
+          targetValue,
+        },
+        period: {
+          startDate,
+          endDate,
+        },
+        progressionList: this.progressCollection,
+        item: this.kpis[0],
+        theme: this.theme,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboardResultIndicatorsSection {
+  padding: 1.5rem;
+  background: var(--color-white);
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+
+    .v-select {
+      min-width: 160px;
+    }
+
+    &::v-deep .vs__dropdown-toggle {
+      .vs__open-indicator {
+        width: 1.5rem;
+        height: 1.5rem;
+      }
+    }
+  }
+}
+</style>
