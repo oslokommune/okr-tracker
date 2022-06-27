608a456f38f9e5dcd117d54a0a3c69fb61feadbb
maynkj <maynkj@protonmail.com>
2022-06-27 15:06:09 +0200

    Add DashboardSection component.

---
+++ src/views/DashboardSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="dashboardSection">
+    <slot name="title" />
+    <slot name="content" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'DashboardSection',
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboardSection {
+  margin: 4rem 0;
+
+  .title-1 {
+    font-weight: 500;
+  }
+}
+</style>
