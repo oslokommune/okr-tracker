e568845e0c47fad1af7c5ee3be3111b6ba0aec48
maynkj <maynkj@protonmail.com>
2022-08-23 16:29:49 +0200

    Add static key figures to dashboard.

---
+++ src/views/Dashboard/data/staticData.js
@@ -0,0 +1,49 @@
+export const POC_DEPARTMENTS = ['apen-by', 'mayn-sitt-omrade'];
+
+export const KEY_FIGURES = [
+  {
+    id: 'reservations',
+    name: {
+      'nb-NO': 'Reservasjoner',
+      'en-US': 'Reservations',
+    },
+    value: 8900,
+    icon: 'IconCalendar',
+  },
+  {
+    id: 'participations',
+    name: {
+      'nb-NO': 'Deltakelser',
+      'en-US': 'Participations',
+    },
+    value: 181000,
+    icon: 'IconOrganization',
+  },
+  {
+    id: 'weddings',
+    name: {
+      'nb-NO': 'Vielser',
+      'en-US': 'Weddings',
+    },
+    value: 2400,
+    icon: 'IconHoldingHands',
+  },
+  {
+    id: 'userProfiles',
+    name: {
+      'nb-NO': 'Brukerprofiler',
+      'en-US': 'User profiles',
+    },
+    value: 106000,
+    icon: 'IconUser',
+  },
+  {
+    id: 'activeUserProfiles',
+    name: {
+      'nb-NO': 'Aktive brukerprofiler',
+      'en-US': 'Active user profiles',
+    },
+    value: 51600,
+    icon: 'IconActivePerson',
+  },
+];
