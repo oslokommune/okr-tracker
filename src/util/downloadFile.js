63418e11fa34077b2e13c363e8189c4908f514cd
Anne M. P <annemerete.pettersen@origo.oslo.kommune.no>
2022-08-24 16:00:32 +0200

    Added downloadoptions for png and svg

---
+++ src/util/downloadFile.js
@@ -0,0 +1,16 @@
+export default function (content, filename, filetype) {
+  const url = URL.createObjectURL(content);
+  const downloadLink = document.createElement('a');
+  if (window.navigator.msSaveOrOpenBlob) {
+    downloadLink.href = '#';
+    downloadLink.download = '';
+    downloadLink.addEventListener('click', () => window.navigator.msSaveOrOpenBlob(content, `${filename}${filetype}`));
+    downloadLink.click();
+  } else {
+    downloadLink.href = url;
+    downloadLink.download = `${filename}${filetype}`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
+}
