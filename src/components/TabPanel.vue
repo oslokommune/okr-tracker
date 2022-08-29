73097b4a7a232243a76e02ca9cada80244fe0a94
maynkj <maynkj@protonmail.com>
2022-08-29 12:21:20 +0200

    Add new tab components.

---
+++ src/components/TabPanel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    :id="tabIds.tabPanel(activeTab)"
+    role="tabpanel"
+    tabindex="0"
+    :aria-labelledby="tabIds.tabButton(activeTab)"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabPanel',
+
+  props: {
+    activeTab: {
+      type: Number,
+      required: true,
+    },
+    tabIds: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
