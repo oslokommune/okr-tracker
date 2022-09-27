590ec3d6f1a0bf4a6499c7dc9e3d29e9234dca78
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2022-09-27 16:11:21 +0200

    Trap focus when modal is open

--- src/components/modals/ModalWrapper.vue
+++ src/components/modals/ModalWrapper.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="overlay" @keydown.esc="close" @click.self="close">
+  <div
+    ref="modalOverlay"
+    class="overlay"
+    @keydown.esc="close"
+    @click.self="close"
+  >
     <div class="modal">
       <div class="modal__header">
         <h2 class="title-2">
@@ -26,20 +31,37 @@
 </template>
 
 <script>
+import * as focusTrap from 'focus-trap';
+
 export default {
   name: 'ModalWrapper',
 
+  data: () => ({
+    focusTrap: null,
+  }),
+
   mounted() {
-    this.$nextTick(() => {
-      this.setElementFocus();
-    });
+    this.$nextTick(this.createFocusTrap);
+  },
+
+  beforeDestroy() {
+    if (this.focusTrap) {
+      this.focusTrap.deactivate();
+      this.focusTrap = null;
+    }
   },
 
   methods: {
     close() {
       this.$emit('close');
     },
-    setElementFocus() {
+    createFocusTrap() {
+      this.focusTrap = focusTrap.createFocusTrap(this.$refs.modalOverlay, {
+        onActivate: this.setInitialFocus,
+      });
+      this.focusTrap.activate();
+    },
+    setInitialFocus() {
       const focusableElement =
         this.$refs.modalContent.querySelectorAll(
           'button, a[href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
@@ -49,9 +71,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.btn--close:focus {
-  outline: 2px solid var(--color-link);
-}
-</style>
