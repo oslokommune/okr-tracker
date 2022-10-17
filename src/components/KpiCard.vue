3ace6e966be2da4a0dfca05c59bc305d9c9b6d6c
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2022-10-17 11:03:21 +0200

    Make KPI grid more responsive

--- src/components/KpiCard.vue
+++ src/components/KpiCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div :class="{ kpi: true }">
-    <div class="kpi__name">{{ kpi.name }}</div>
-    <div class="kpi__value">
+  <div class="kpi-card">
+    <div class="kpi-card__name">{{ kpi.name }}</div>
+    <div class="kpi-card__value">
       <span v-if="kpi.error || !kpi.valid">–––</span>
       <span v-else>{{ formatKPIValue(kpi) }}</span>
     </div>
@@ -9,11 +9,10 @@
 </template>
 
 <script>
-import { numberLocale } from '@/util';
 import { formatKPIValue } from '@/util/kpiHelpers';
 
 export default {
-  name: 'KPI',
+  name: 'KpiCard',
 
   props: {
     kpi: {
@@ -30,13 +29,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.kpi {
+.kpi-card {
   position: relative;
-  height: 8rem;
-  padding: 1rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  gap: 0.5rem;
+  padding: 1rem;
   overflow: hidden;
   color: var(--color-text);
   font-weight: 500;
@@ -44,8 +43,16 @@ export default {
 
   &__value {
     font-weight: 900;
-    font-size: 3rem;
+    font-size: 1.5rem;
     line-height: 1em;
+  }
+
+  @media screen and (min-width: bp(m)) {
+    gap: 1rem;
+
+    &__value {
+      font-size: 2rem;
+    }
   }
 }
 </style>
