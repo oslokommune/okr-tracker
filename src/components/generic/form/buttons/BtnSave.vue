328f6627efdf4ceb780b713a8a90b7f406d31a79
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2022-10-31 15:45:25 +0100

    Add reusable button components

---
+++ src/components/generic/form/buttons/BtnSave.vue
@@ -0,0 +1,28 @@
+<script>
+import Btn from './Btn.vue';
+
+export default {
+  name: 'BtnSave',
+  extends: Btn,
+
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default() {
+        return this.$t('btn.save');
+      },
+    },
+    variant: {
+      type: String,
+      required: false,
+      default: 'confirm',
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: 'save',
+    },
+  },
+};
+</script>
---
+++ src/components/generic/form/buttons/index.js
@@ -0,0 +1,5 @@
+import Btn from './Btn.vue';
+import BtnDelete from './BtnDelete.vue';
+import BtnSave from './BtnSave.vue';
+
+export { Btn, BtnDelete, BtnSave };
