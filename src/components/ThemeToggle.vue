f64d7c06b3ad5d9484c64a1873b6163628fc591a
thomasdtran <thian0703@gmail.com>
2022-11-02 08:33:21 +0100

    Fixed timeout of running emulators locally
    added "host": "127.0.0.1" to all the emulators in the firebase.json file

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <h2 class="widget__title">{{ $t('theme.header') }}</h2>
-
     <div v-for="mode in modes" :key="mode.id" class="ods-form-group">
       <input
-        :id="mode.id"
         type="radio"
+        :id="mode.id"
         class="ods-form-radio"
         name="radio-group"
         :checked="theme === mode.id"
@@ -17,6 +16,7 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
 
 export default {
   name: 'ThemeToggle',
@@ -30,46 +30,17 @@ export default {
   },
 
   data: () => ({
-    mode: 'knowit',
+    selectedMode: 'knowit',
+    modes: [{ id: 'blue' }, { id: 'knowit' }],
   }),
 
   computed: {
-    nextThemeMode() {
-      if (this.mode === 'yellow') {
-        return 'blue';
-      }
-      if (this.mode === 'blue') {
-        return 'green';
-      }
-      if (this.mode === 'green') {
-        return 'knowit';
-      }
-      return 'yellow';
-    },
-  },
-
-  mounted() {
-    if (this.hasInStorage()) {
-      this.mode = this.getLocalThemeMode();
-      this.setThemeMode();
-    } else {
-      this.mode = 'knowit';
-      this.setThemeMode();
-    }
+    ...mapState(['theme', 'LS_MODE']),
   },
 
   methods: {
-    handleClick() {
-      if (this.mode === 'yellow') {
-        this.mode = 'blue';
-      } else if (this.mode === 'blue') {
-        this.mode = 'green';
-      } else if (this.mode === 'green') {
-        this.mode = 'knowit';
-      } else {
-        this.mode = 'yellow';
-      }
-    }, 
+    ...mapActions(['setTheme']),
+
     setThemeMode(mode) {
       this.saveThemeMode(mode);
       this.setTheme(mode);
