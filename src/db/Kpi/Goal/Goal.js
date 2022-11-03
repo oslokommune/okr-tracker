0575cf626f6311409635213ffc2352e14ef8c00c
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2022-11-03 09:10:22 +0100

    Add database interface for KPI goals

---
+++ src/db/Kpi/Goal/Goal.js
@@ -0,0 +1,41 @@
+import { db } from '@/config/firebaseConfig';
+import props from './props';
+import { validateCreateProps, validateUpdateProps } from '../../common';
+
+async function create(kpiId, data) {
+  if (!validateCreateProps(props, data)) {
+    throw new Error('Invalid data');
+  }
+
+  const { ref } = await db.collection('kpis').doc(kpiId).get();
+  const goalsRef = ref.collection('goals');
+
+  return goalsRef.add(data);
+};
+
+async function get(kpiId, goalId) {
+  const { ref } = await db.collection('kpis').doc(kpiId).get();
+  return ref.collection('goals').doc(goalId);
+};
+
+async function update(kpiId, goalId, data) {
+  validateUpdateProps(props, data);
+
+  const goal = await get(kpiId, goalId);
+
+  return goal.update(data);
+};
+
+async function archive(kpiId, goalId) {
+  const goal = await get(kpiId, goalId);
+
+  return goal.update({ archived: true });
+};
+
+async function restore(kpiId, goalId) {
+  const goal = await get(kpiId, goalId);
+
+  return goal.update({ archived: false });
+};
+
+export default { create, get, update, archive, restore };
