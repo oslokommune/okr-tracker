5177f5c9e35d7ee6c4f09e8af9c0fdd65be30d94
thomasdtran <thian0703@gmail.com>
2022-11-23 14:45:27 +0100

    Add missing code in api routes and helpers

--- functions/api/index.js
+++ functions/api/index.js
@@ -10,17 +10,18 @@ import config from '../config.js';
 // Routes
 import kpiRoutes from './routes/kpi.js';
 import keyResRoutes from './routes/keyres.js';
+import statusRoutes from './routes/status.js';
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan('combined'));
 
 app.use('/kpi', kpiRoutes);
-
 app.use('/keyres', keyResRoutes);
+app.use('/status', statusRoutes);
 
 const api = functions.runWith(config.runtimeOpts).region(config.region).https.onRequest(app);
 
