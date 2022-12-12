f47b73baabff0823376bd90add6071f4a93dbfa2
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2022-12-12 13:39:10 +0100

    Add reusable form validation observer component

---
+++ src/components/generic/form/index.js
@@ -0,0 +1,5 @@
+import FormSection from './FormSection.vue';
+import ToggleButton from './ToggleButton.vue';
+import { Btn, BtnDelete, BtnSave } from './buttons';
+
+export { FormSection, ToggleButton, Btn, BtnDelete, BtnSave };
