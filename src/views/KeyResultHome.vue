4e3e1a90cf4f813a0fc6706afb32ec840ed9f7d6
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2023-02-15 15:31:30 +0100

    Refactor key result home view

--- src/views/KeyResultHome.vue
+++ src/views/KeyResultHome.vue
@@ -9,119 +9,110 @@
         </p>
       </header>
 
-      <section>
-        <div class="key-result-row">
-          <div class="key-result-row__progress">
-            <h3 class="key-result-row__progress--header">
-              {{ $t('keyResult.registerProgression.value') }} ({{ activeKeyResult.unit }})
-            </h3>
-            <widget-key-result-progress-details
-              :progress-details="progressDetails"
-              :unit="activeKeyResult.unit"
-            />
-            <div class="progression__total">
-              <span class="progression__total--current">
-                {{ progressDetails.formattedTotalCompletedTasks }}
-              </span>
-              <span class="progression__total--target">
-                {{
-                  $t('progress.remainingOf', {
-                    progress: progressDetails.formattedTotalNumberOfTasks,
-                  })
-                }}
-              </span>
-            </div>
-            <progress-bar
-              :progression="progressDetails.percentageCompleted"
-              :is-compact="false"
-              :dark="true"
-              class="key-result-row__progressBar"
-            />
-          </div>
-
-          <div class="key-result-row__info">
-            <h3 class="title-3">{{ activeKeyResult.name }}</h3>
-            <span class="key-result-row__info--description">
-              {{ activeKeyResult.description }}
+      <section class="key-result-row">
+        <div class="key-result-row__progress">
+          <h3 class="key-result-row__progress--header">
+            {{ $t('keyResult.registerProgression.value') }} ({{ activeKeyResult.unit }})
+          </h3>
+          <widget-key-result-progress-details
+            :progress-details="progressDetails"
+            :unit="activeKeyResult.unit"
+          />
+          <div class="progression__total">
+            <span class="progression__total--current">
+              {{ progressDetails.formattedTotalCompletedTasks }}
+            </span>
+            <span class="progression__total--target">
+              {{
+                $t('progress.remainingOf', {
+                  progress: progressDetails.formattedTotalNumberOfTasks,
+                })
+              }}
             </span>
           </div>
-
-          <div class="key-result__value">
-            <div>
-              <h3 class="title-2">{{ $t('keyResult.newValue') }}</h3>
-
-              <validation-observer v-slot="{ handleSubmit }">
-                <form id="modal" @submit.prevent="handleSubmit(saveProgress)">
-                  <label>
-                    <span class="title-4">{{ $t('keyResult.addComment') }}</span>
-                    <textarea
-                      v-model="progressNote"
-                      style="margin-top: 0.5rem"
-                      rows="3"
-                      :placeholder="$t('keyResult.commentPlaceholder')"
-                      :disabled="!allowedToEditPeriod"
-                    />
-                  </label>
-
-                  <div>
-                    <validation-provider
-                      v-slot="{ errors }"
-                      name="value"
-                      rules="required"
-                    >
-                      <label class="form-group">
-                        <span class="form-label">{{ $t('keyResult.newValue') }}</span>
-                        <input
-                          v-model="value"
-                          style="margin-top: 0.25rem"
-                          type="number"
-                          step="any"
-                          :disabled="!allowedToEditPeriod"
-                        />
-                        <span class="form-field--error">{{ errors[0] }}</span>
-                      </label>
-                    </validation-provider>
-                  </div>
-                </form>
-              </validation-observer>
-            </div>
-
-            <btn-save
-              v-if="allowedToEditPeriod"
-              form="modal"
-              variant="primary"
-              class="key-result__value--button"
-              :disabled="isSaving"
-            />
-          </div>
-
-          <div class="key-result__graph">
-            <h2 class="title-2">
-              {{ $t('objective.progression') }}
-            </h2>
-
-            <svg ref="graph" class="graph"></svg>
-          </div>
+          <progress-bar
+            :progression="progressDetails.percentageCompleted"
+            :is-compact="false"
+            :dark="true"
+            class="key-result-row__progressBar"
+          />
         </div>
 
-        <pkt-alert v-if="activeKeyResult.auto" skin="info">
-          <template #content>{{ $t('keyResult.autoHelpText') }}</template>
-        </pkt-alert>
+        <div class="key-result-row__info">
+          <h3 class="title-3">{{ activeKeyResult.name }}</h3>
+          <span class="key-result-row__info--description">
+            {{ activeKeyResult.description }}
+          </span>
+        </div>
 
-        <pkt-alert v-if="activeKeyResult.auto && activeKeyResult.error" skin="error">
-          <template #content>
-            {{ $t('keyResult.autoError') }} {{ activeKeyResult.error }}
-          </template>
-        </pkt-alert>
+        <div class="key-result__value">
+          <div>
+            <h3 class="title-2">{{ $t('keyResult.newValue') }}</h3>
 
-        <widget-progress-history
-          :progress="progress"
-          :is-loading="isLoading"
-          :no-values-message="$t('empty.noKeyResultProgress')"
-          @update-record="updateHistoryRecord"
-          @delete-record="deleteHistoryRecord"
-        />
+            <validation-observer v-slot="{ handleSubmit }">
+              <form id="modal" @submit.prevent="handleSubmit(saveProgress)">
+                <label>
+                  <span class="title-4">{{ $t('keyResult.addComment') }}</span>
+                  <textarea
+                    v-model="progressNote"
+                    style="margin-top: 0.5rem"
+                    rows="3"
+                    :placeholder="$t('keyResult.commentPlaceholder')"
+                    :disabled="!allowedToEditPeriod"
+                  />
+                </label>
+
+                <div>
+                  <validation-provider v-slot="{ errors }" name="value" rules="required">
+                    <label class="form-group">
+                      <span class="form-label">{{ $t('keyResult.newValue') }}</span>
+                      <input
+                        v-model="value"
+                        style="margin-top: 0.25rem"
+                        type="number"
+                        step="any"
+                        :disabled="!allowedToEditPeriod"
+                      />
+                      <span class="form-field--error">{{ errors[0] }}</span>
+                    </label>
+                  </validation-provider>
+                </div>
+              </form>
+            </validation-observer>
+          </div>
+
+          <btn-save
+            v-if="allowedToEditPeriod"
+            form="modal"
+            variant="primary"
+            class="key-result__value--button"
+            :disabled="isSaving"
+          />
+        </div>
       </section>
+
+      <section class="key-result-graph">
+        <h3 class="title-2">{{ $t('objective.progression') }}</h3>
+        <svg ref="graph" class="graph"></svg>
+      </section>
+
+      <pkt-alert v-if="activeKeyResult.auto" skin="info">
+        <template #content>{{ $t('keyResult.autoHelpText') }}</template>
+      </pkt-alert>
+
+      <pkt-alert v-if="activeKeyResult.auto && activeKeyResult.error" skin="error">
+        <template #content>
+          {{ $t('keyResult.autoError') }} {{ activeKeyResult.error }}
+        </template>
+      </pkt-alert>
+
+      <widget-progress-history
+        :progress="progress"
+        :is-loading="isLoading"
+        :no-values-message="$t('empty.noKeyResultProgress')"
+        @update-record="updateHistoryRecord"
+        @delete-record="deleteHistoryRecord"
+      />
     </main>
 
     <widgets-right />
@@ -297,7 +288,7 @@ export default {
 @use '@/styles/typography';
 
 .pkt-alert {
-  margin-bottom: 0.5rem;
+  margin-bottom: 1rem;
 }
 
 .key-result-row {
@@ -310,7 +301,6 @@ export default {
     grid-column-gap: 1px;
     grid-template-rows: repeat(3, auto);
     grid-template-columns: 1fr span(3, span(8));
-    margin-bottom: 0.5rem;
   }
 }
 
@@ -374,8 +364,7 @@ export default {
   font-size: typography.$font-size-0;
 }
 
-.key-result__graph {
-  grid-area: 3 / 1 / 4 / 3;
+.key-result-graph {
   padding: 1.5rem 1.75rem;
   background-color: var(--color-white);
 }
