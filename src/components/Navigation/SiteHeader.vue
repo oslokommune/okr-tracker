b26d82ca94a2e41a0bd9b25690fc564743b0a86c
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2023-05-11 15:47:20 +0200

    Add menu to site header

--- src/components/Navigation/SiteHeader.vue
+++ src/components/Navigation/SiteHeader.vue
@@ -1,54 +1,91 @@
 <template>
   <header class="site-header">
-    <button class="btn btn--ter btn-pri" @click="toggleShowAsideLeft">
-      <pkt-icon name="menu" />
-    </button>
-    <h1 v-if="title" class="site-header__title">
-      {{ title }}
-    </h1>
-    <button
-      class="btn btn--ter btn--pri"
-      :class="{ hidden: !user }"
-      @click="toggleShowAsideRight"
-    >
-      <pkt-icon name="user" />
-    </button>
+    <menu-component mode="horizontal">
+      <!-- Site menu dropdown -->
+      <menu-item variant="dropdown" class="menu-item--site">
+        <template #label>
+          <span class="logo">
+            <img
+              alt="Oslo kommune logo"
+              src="@oslokommune/punkt-assets/dist/logos/oslologo.svg"
+            />
+          </span>
+          <span class="pkt-show-phablet-up">{{ siteMenuLabel }}</span>
+        </template>
+        <template #dropdown="{ close }">
+          <site-sidebar :handle-navigation="close" />
+        </template>
+      </menu-item>
+
+      <!-- Item tabs -->
+      <template v-if="activeItem && $route.params?.slug">
+        <menu-item
+          v-for="(itemMenuTab, index) in itemTabs"
+          :key="`item_tabs_${index}`"
+          variant="tab"
+          v-bind="itemMenuTab"
+        >
+          <template #label>
+            <span class="pkt-show-tablet-big-up">{{ itemMenuTab.label }}</span>
+            <span class="pkt-hide-tablet-big-up">
+              {{ itemMenuTab.shortLabel || itemMenuTab.label }}
+            </span>
+          </template>
+        </menu-item>
+      </template>
+
+      <!-- User menu dropdown -->
+      <menu-item
+        v-if="user"
+        ref="userMenu"
+        variant="dropdown"
+        icon="user"
+        class="menu-item--user"
+      >
+        <template #dropdown="{ close }">
+          <user-profile-menu :id="user.id" :handle-navigation="close" />
+        </template>
+      </menu-item>
+    </menu-component>
   </header>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 import i18n from '@/locale/i18n';
+import MenuComponent from '@/components/menu/MenuComponent.vue';
+import MenuItem from '@/components/menu/MenuItem.vue';
+import SiteSidebar from '@/components/Navigation/SiteSidebar.vue';
+import UserProfileMenu from '@/components/UserProfileMenu.vue';
+import getActiveItemType from '@/util/getActiveItemType';
 
 export default {
   name: 'SiteHeader',
 
-  props: {
-    toggleShowAsideLeft: {
-      type: Function,
-      required: true,
-    },
-    toggleShowAsideRight: {
-      type: Function,
-      required: true,
-    },
+  components: {
+    MenuComponent,
+    MenuItem,
+    SiteSidebar,
+    UserProfileMenu,
   },
 
   computed: {
-    ...mapState(['activeItem', 'user']),
+    ...mapState(['activeItem', 'activeKeyResult', 'activeObjective', 'user']),
 
-    /**
-     * Return the page title based on the current route.
-     */
-    title() {
+    siteMenuLabel() {
+      const defaultLabel = i18n.t('general.appName') || 'OKR Tracker';
       const parts = this.$route.matched.map(({ name }) => name);
 
       if (parts.includes('Admin')) {
-        return i18n.t('general.admin');
+        return `${defaultLabel}: ${i18n.t('general.admin')}`;
       }
 
       if (parts.includes('Help')) {
-        return i18n.t('general.help');
+        return `${defaultLabel}: ${i18n.t('general.help')}`;
+      }
+
+      if (parts.includes('Api')) {
+        return `${defaultLabel}: ${i18n.t('general.api')}`;
       }
 
       if (
@@ -63,13 +100,65 @@ export default {
         return this.activeItem.name;
       }
 
-      return 'OKR Tracker';
+      return defaultLabel;
+    },
+
+    itemTabs() {
+      return [
+        {
+          route: { name: 'ItemHome' },
+          label: this.$t('general.OKRsLong'),
+          shortLabel: this.$t('general.OKRs'),
+        },
+        {
+          route: { name: 'ItemMeasurements' },
+          label: this.$t('general.KPIs'),
+        },
+        {
+          route: { name: 'ItemAbout' },
+          label: this.aboutLabel,
+          shortLabel: this.$t('about.about'),
+        },
+        {
+          show: this.hasEditRights,
+          route: { name: 'ItemAdmin', query: this.adminLinkQuery },
+          label: this.$t('general.admin'),
+        },
+      ];
+    },
+
+    aboutLabel() {
+      switch (getActiveItemType(this.activeItem)) {
+        case 'organization':
+          return this.$t('about.aboutOrganization');
+        case 'department':
+          return this.$t('about.aboutDepartment');
+        case 'product':
+          return this.$t('about.aboutProduct');
+        default:
+          return this.$t('about.about');
+      }
+    },
+
+    adminLinkQuery() {
+      switch (this.$route.name) {
+        case 'ItemHome':
+          return { tab: 'okr' };
+        case 'ObjectiveHome':
+          return { tab: 'okr', type: 'objective', id: this.activeObjective?.id };
+        case 'KeyResultHome':
+          return { tab: 'okr', type: 'keyResult', id: this.activeKeyResult?.id };
+        case 'ItemMeasurements':
+          return { tab: 'kpi' };
+        default:
+          return {};
+      }
     },
   },
 
   mounted() {
     if (this.user && this.user.position === null) {
-      this.toggleShowAsideRight();
+      this.$refs.userMenu.activate();
     }
   },
 };
@@ -78,33 +167,41 @@ export default {
 <style lang="scss" scoped>
 .site-header {
   display: flex;
-  flex: 0 0 4rem;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 0.6875rem;
-  color: var(--color-text-secondary);
-  background: var(--color-primary);
+  flex: 0 0 3.5rem;
 
-  &__title {
-    font-weight: 500;
-    font-size: 1.25rem;
-
-    @media screen and (min-width: bp(s)) {
-      font-size: 1.5rem;
-    }
-
-    @media screen and (min-width: bp(m)) {
-      font-size: 1.65rem;
-    }
+  .menu {
+    flex-basis: 100%;
+    height: 100%;
   }
 
-  svg {
-    --fg-color: var(--color-white);
-    height: 1.75rem;
-  }
-}
+  .menu-item {
+    &--site {
+      border-right: 1px solid var(--color-gray);
 
-.hidden {
-  visibility: hidden;
+      ::v-deep .menu-item__inner {
+        text-shadow: 0.25px 0px 0.1px, -0.25px 0px 0.1px;
+      }
+
+      .logo {
+        height: 1.5rem;
+        width: 1.5rem;
+        line-height: 1;
+        overflow: hidden;
+
+        img {
+          height: 100%;
+        }
+      }
+    }
+
+    &--user {
+      margin-left: auto;
+      border-left: 1px solid var(--color-gray);
+
+      ::v-deep .menu-item__content {
+        right: 0;
+      }
+    }
+  }
 }
 </style>
