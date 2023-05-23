oslokommune/okr-tracker

commit 7263eb1a124d7f5ca8eb87c90e9b8df2e6fa2377

Plot the initial value of OKR key results

src/util/LineChart/index.js
@@ -78,10 +78,10 @@ export default class LineChart {
    * Render the graph. Takes the following parameters:
    *
    * `startValue`: Optional. The start value of the y axis. Will start at the
-   *     lowest value present in `progress` if unset.
+   *   lowest value present in `progress` if unset.
    *
    * `targetValue`: Optional. The end value of the y axis. Will end at the
-   *     highest value present in `progress` if unset.
+   *   highest value present in `progress` if unset.
    *
    * `startDate`: The first date on the x axis.
    *
@@ -93,8 +93,20 @@ export default class LineChart {
    *   object specifying `startDate, `endDate`, and `value`.
    *
    * `kpi`: Optional. A KPI to format the y axis for.
+   *
+   * `initialValue`: Optional. If given, plot a value `initialValue` at
+   *   `startDate`.
    */
-  render({ startValue, targetValue, startDate, endDate, progress, targets, kpi }) {
+  render({
+    startValue,
+    targetValue,
+    startDate,
+    endDate,
+    progress,
+    targets,
+    kpi,
+    initialValue,
+  }) {
     let fromValue = startValue;
     let toValue = targetValue;
 
@@ -179,16 +191,13 @@ export default class LineChart {
       .map((d) => {
         const timestamp = d.timestamp.toDate();
         timestamp.setHours(0, 0, 0, 0);
-
-        return {
-          timestamp,
-          value: +d.value,
-          comment: d?.comment,
-          kpi,
-          startValue: +fromValue,
-        };
+        return { timestamp, value: +d.value, comment: d?.comment };
       })
       .sort((a, b) => (a.timestamp > b.timestamp ? 1 : -1));
+
+    if (data.length && typeof initialValue === 'number') {
+      data.unshift({ timestamp: _startDate, value: initialValue });
+    }
 
     this.canvas.selectAll('.single-point').remove();
 

src/views/KeyResultHome.vue
@@ -193,6 +193,7 @@ export default {
         startDate,
         endDate,
         progress: this.progress,
+        initialValue: startValue,
       });
     },
 

commit b9f13f73213683837489183aa8037e0b4af553b6

Fix formatting of line chart tooltips
Regression introduced in 7263eb1a124d7f5ca8eb87c90e9b8df2e6fa2377.

src/util/LineChart/index.js
@@ -191,7 +191,7 @@ export default class LineChart {
       .map((d) => {
         const timestamp = d.timestamp.toDate();
         timestamp.setHours(0, 0, 0, 0);
-        return { timestamp, value: +d.value, comment: d?.comment };
+        return { timestamp, value: +d.value, comment: d?.comment, kpi };
       })
       .sort((a, b) => (a.timestamp > b.timestamp ? 1 : -1));
 