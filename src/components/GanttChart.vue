oslokommune/okr-tracker

commit 78fb429664d9d999d071f145ba16bf1f55e8367c

Change the background to white throughout the app

src/components/GanttChart.vue
@@ -245,5 +245,6 @@ export default {
   position: relative;
   z-index: 1;
   margin: 1rem 0;
+  border: 2px solid var(--color-border);
 }
 </style>

src/App.vue
@@ -57,6 +57,5 @@ window.noZensmooth = true;
 <style lang="scss" scoped>
 .app {
   min-height: 100vh;
-  background: var(--color-gray);
 }
 </style>

src/components/ContentLoader/ContentLoaderItem.vue
@@ -36,8 +36,7 @@ export default {
 .content-loader-item__objective-container {
   margin-bottom: 1rem;
   padding-bottom: 1rem;
-  background: white;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border: 2px solid var(--color-border);
 }
 
 .content-loader-item__objective {

src/components/KeyResultRow.vue
@@ -115,8 +115,6 @@ export default {
 @use '@/styles/typography';
 
 .keyResult {
-  background: white;
-
   @media screen and (min-width: bp(s)) {
     display: flex;
   }
@@ -141,6 +139,7 @@ export default {
 
   &__title {
     margin-bottom: 0;
+    font-weight: 400;
   }
 
   &__description {

src/components/KpiWidgetGroup.vue
@@ -153,6 +153,10 @@ export default {
     &.router-link-active {
       border-color: var(--color-hover);
 
+      .kpi-card-widget {
+        border-color: var(--color-white);
+      }
+
       ::v-deep .widget__header h3 {
         color: var(--color-hover);
       }

src/components/ObjectiveRow.vue
@@ -60,7 +60,6 @@ export default {
   padding: 1.5rem 1.5rem 1.25rem 1.5rem;
   color: var(--color-text);
   text-decoration: none;
-  background: var(--color-white);
 
   .title-2 {
     line-height: 1.25;

src/components/modals/EditGoalsModal.vue
@@ -298,7 +298,7 @@ export default {
     flex-direction: column;
     flex-grow: 1;
     height: 100%;
-    border: 1px solid var(--color-grayscale-10);
+    border: 2px solid var(--color-border);
 
     @media screen and (min-width: bp(s)) {
       margin-right: 1.5rem;
@@ -313,7 +313,7 @@ export default {
       display: block;
       padding: 0.5rem 0.75rem;
       text-decoration: none;
-      border-bottom: 1px solid var(--color-grayscale-10);
+      border-bottom: 2px solid var(--color-border);
       cursor: pointer;
 
       &.selected {
@@ -328,7 +328,7 @@ export default {
 
     button {
       margin-top: auto;
-      border-top: 1px solid var(--color-grayscale-10);
+      border-top: 2px solid var(--color-border);
     }
 
     .icon {

src/components/widgets/WidgetWrapper.vue
@@ -81,7 +81,7 @@ export default {
   width: 100%;
   margin-bottom: 1rem;
   padding: 1.5rem;
-  background-color: var(--color-white);
+  border: 2px solid var(--color-border);
 
   &__toggle {
     position: absolute;

commit dcd19bce7c8074382aea7ae1c8c27b4ced2e7e86

Fix today indicator styling in the timeline chart
Regression introduced in 78fb429664d9d999d071f145ba16bf1f55e8367c.

src/components/GanttChart.vue
@@ -245,6 +245,7 @@ export default {
   position: relative;
   z-index: 1;
   margin: 1rem 0;
+  background: var(--color-white);
   border: 2px solid var(--color-border);
 }
 </style>