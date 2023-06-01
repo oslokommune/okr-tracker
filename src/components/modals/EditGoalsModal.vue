oslokommune/okr-tracker

commit fe1b70fedb6c848bf83d54181ae8c5c794438460

Use Punkt's typography module
Load Punkt's `typography` module and adjust our existing styling
accordingly.

src/components/modals/EditGoalsModal.vue
@@ -312,26 +312,17 @@ export default {
     li {
       display: block;
       padding: 0.5rem 0.75rem;
-      color: var(--color-text);
       text-decoration: none;
       border-bottom: 1px solid var(--color-grayscale-10);
       cursor: pointer;
 
       &.selected {
-        color: var(--color-text);
         font-weight: 500;
         background: var(--color-gray-light);
-      }
-
-      &.active {
-        color: var(--color-text-secondary);
-        font-weight: 500;
-        background: var(--color-primary);
       }
     }
 
     a {
-      color: var(--color-text);
       text-decoration: none;
     }
 

index.html
@@ -137,10 +137,10 @@
 
   <body>
     <noscript>
-      <strong class="font-weight-medium"
-        >We're sorry but okr-tracker doesn't work properly without JavaScript
-        enabled. Please enable it to continue.</strong
-      >
+      <strong>
+        We're sorry but okr-tracker doesn't work properly without JavaScript
+        enabled. Please enable it to continue.
+      </strong>
     </noscript>
     <div id="spinner" class="spinner__wrapper">
       <div class="lds-default">

src/components/ItemRow.vue
@@ -226,7 +226,6 @@ export default {
   display: inline-block;
   margin-left: auto;
   padding: 0 0.5rem;
-  color: var(--color-text);
   text-align: center;
 }
 

src/components/KeyResultProgressDetails.vue
@@ -48,13 +48,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.keyResultProgressDetails {
-  color: var(--color-text);
-
-  &__infoMessage {
-    &:not(:last-child) {
-      margin-bottom: 0.25rem;
-    }
-  }
+.keyResultProgressDetails__infoMessage:not(:last-child) {
+  margin-bottom: 0.25rem;
 }
 </style>

src/components/KeyResultRow.vue
@@ -190,8 +190,6 @@ export default {
   }
 
   &__progressionSummary {
-    color: var(--color-blue-dark);
-    font-weight: 300;
     text-align: right;
   }
 }

src/components/Navigation/SiteSidebar.vue
@@ -168,6 +168,7 @@ $header-height: 4em;
   display: flex;
   flex-direction: column;
   max-height: calc(100vh - 4rem);
+  line-height: 1;
 }
 
 .sidebar__item {
@@ -184,8 +185,13 @@ $header-height: 4em;
     height: 1.5rem;
   }
 
+  &:visited {
+    color: var(--color-text-secondary);
+  }
+
   &:hover {
     color: var(--color-text);
+    text-decoration: none;
     background-color: var(--color-secondary) !important;
 
     svg {

src/components/ObjectiveRow.vue
@@ -7,7 +7,7 @@
       <span>{{ objective.name }}</span>
       <span>{{ percent(objective.progression) }}</span>
     </h3>
-    <p v-if="showDescription && objective.description" class="objective__description">
+    <p v-if="showDescription && objective.description">
       {{ objective.description }}
     </p>
     <progress-bar v-if="showProgress" :progression="objective.progression * 100" />
@@ -80,9 +80,5 @@ export default {
   gap: 1.5rem;
   justify-content: space-between;
   margin-bottom: 0;
-}
-
-.objective__description {
-  line-height: 1.5rem;
 }
 </style>

src/components/RoleMembers.vue
@@ -53,9 +53,5 @@ export default {
     margin-bottom: 0.25rem;
     color: var(--color-grayscale-30);
   }
-
-  &__members-list {
-    line-height: 1.5rem;
-  }
 }
 </style>

commit a44cc4df1657f29009db8c137133c623a0b88dfa

Fix link color in goal edit modal
Regression introduced in fe1b70fedb6c848bf83d54181ae8c5c794438460, do
a partly revert.

src/components/modals/EditGoalsModal.vue
@@ -317,6 +317,7 @@ export default {
     }
 
     a {
+      color: var(--color-text);
       text-decoration: none;
     }
 