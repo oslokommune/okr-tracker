bd70cdc9d9636feacdc4b95bedcc1d100d11bc0e
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2023-06-07 12:49:25 +0200

    Unify OKR and measurements date pickers

--- src/router/router-guards/index.js
+++ src/router/router-guards/index.js
@@ -3,6 +3,7 @@ export { default as home } from './home';
 export { default as itemAdmin } from './itemAdmin';
 export { default as itemCommon } from './itemCommon';
 export { default as itemMeasurements } from './itemMeasurements';
+export { default as itemOKRs } from './itemOKRs';
 export { default as keyResultHome } from './keyResultHome';
 export { default as login } from './login';
 export { default as objectiveHome } from './objectiveHome';
