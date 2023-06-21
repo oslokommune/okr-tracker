43efb3aefe944b118997adbf454ed75c52f0e23c
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2023-06-21 09:24:43 +0200

    Add key result list component

---
+++ src/components/KeyResultsList.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="key-results-list">
+    <li v-for="keyResult in keyResults" :key="keyResult.id" class="key-results-list__row">
+      <key-result-row :key-result="keyResult" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'KeyResultsList',
+
+  components: {
+    KeyResultRow: () => import('@/components/KeyResultRow.vue'),
+  },
+
+  props: {
+    keyResults: {
+      type: Array,
+      required: false,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.key-results-list {
+  // margin: 1.5rem 0;
+
+  &__row {
+    margin-top: 0.25rem;
+
+    &:first-child {
+      margin-top: 0;
+    }
+  }
+}
+</style>
--- src/views/ObjectiveHome.vue
+++ src/views/ObjectiveHome.vue
@@ -37,22 +37,14 @@
       </header>
 
       <section>
+        <key-results-list v-if="keyRes.length" :key-results="keyRes" />
+
         <empty-state
-          v-if="!keyRes.length"
+          v-else
           :icon="'poop'"
           :heading="$t('empty.noKeyResults.heading')"
           :body="$t('empty.noKeyResults.body')"
-        >
-        </empty-state>
-
-        <div class="key-results__list">
-          <key-result-row
-            v-for="keyResult in keyRes"
-            :key="keyResult.id"
-            :key-result="keyResult"
-            class="key-results__list--row"
-          />
-        </div>
+        />
       </section>
     </template>
 
@@ -87,7 +79,7 @@ export default {
   name: 'ObjectiveHome',
 
   components: {
-    KeyResultRow: () => import('@/components/KeyResultRow.vue'),
+    KeyResultsList: () => import('@/components/KeyResultsList.vue'),
     Widget: WidgetWrapper,
     WidgetWeights,
     WidgetObjectiveDetails,
@@ -165,23 +157,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.key-results__list {
-  margin: 1.5rem 0;
-}
-
-.key-results__list--row {
-  margin-top: 4px;
-
-  &:first-child {
-    margin-top: 0;
-  }
-}
-
-.key-result__new {
-  display: flex;
-  justify-content: flex-start;
-}
-
 .objective__heading {
   display: flex;
   flex-direction: row;
@@ -204,5 +179,9 @@ export default {
 
 .objective__add-key-res {
   margin-left: auto;
+}
+
+.key-results-list {
+  margin-top: 1.5rem;
 }
 </style>
