43e6c4d78b9834b86dbbb9ea3bc90cf4df3079b1
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2023-06-22 09:21:13 +0200

    Use compact dates in the timeline header

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,6 @@
 export { dateLong } from './format';
 export { dateLongCompact } from './format';
 export { dateShort } from './format';
-export { dateMonthYear } from './format';
 export { dateTimeShort } from './format';
 export { dateTimeLong } from './format';
 export { periodDates } from './format';
@@ -13,5 +12,4 @@ export { default as tableOfContent } from './tableOfContent';
 export { default as toastArchiveAndRevert } from './toastUtils';
 export { default as validateEmail } from './validateEmail';
 
-export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 export const getRandomInt = (max) => Math.floor(Math.random() * max);
