0a416a39bfa8fcf65ff15da1138e0c5f2e5d54c6
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2023-07-07 14:36:36 +0200

    Use `NotFoundPage` in the `NotFound` view

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,20 +1,15 @@
 <template>
-  <empty-page :heading="$t('notFound.title')" :body="$t('notFound.body')" skin="warning">
-    <router-link :to="{ name: 'Home' }" class="pkt-link">
-      <pkt-icon class="pkt-link__icon" name="chevron-left" />
-      {{ $t('notFound.linkText') }}
-    </router-link>
-  </empty-page>
+  <not-found-page back-to="Home" />
 </template>
 
 <script>
 import i18n from '@/locale/i18n';
-import EmptyPage from '@/components/pages/EmptyPage.vue';
+import NotFoundPage from '@/components/pages/NotFoundPage.vue';
 
 export default {
   name: 'NotFound',
   components: {
-    EmptyPage,
+    NotFoundPage,
   },
 
   metaInfo() {
