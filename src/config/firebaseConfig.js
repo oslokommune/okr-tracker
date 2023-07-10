d981199b73d813c36b331ac35dc4c2b297a562b9
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2023-07-10 14:41:11 +0200

    Upgrade to Firebase SDK version 9

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/storage';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/functions';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/functions';
 
 import { emulators } from '../../firebase.json';
 
 const firestoreEmulator = {
   host: `localhost:${emulators.firestore.port}`,
   ssl: false,
-  experimentalForceLongPolling: true,
+  experimentalAutoDetectLongPolling: true,
   merge: true,
 };
 
--- src/db/Progress/Progress.js
+++ src/db/Progress/Progress.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 import props from './props';
 import {
   createDocument,
