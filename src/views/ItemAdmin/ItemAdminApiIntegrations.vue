07e3487e9187d9efef3f47df34aad6439839852c
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2023-08-10 16:49:00 +0200

    Add functionality for creating API clients

---
+++ src/views/ItemAdmin/ItemAdminApiIntegrations.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="integrations">
+    <h1 class="pkt-txt-24-medium">{{ $t('general.integrations') }}</h1>
+    <i18n path="integration.info" tag="p">
+      <template #clientIdHeader>
+        <code>okr-client-id</code>
+      </template>
+      <template #clientSecretHeader>
+        <code>okr-client-secret</code>
+      </template>
+      <template #apiKeyHeader>
+        <code>x-api-key</code>
+      </template>
+    </i18n>
+
+    <div v-if="dataLoading"><loading-small /> {{ $t('general.loading') }}</div>
+
+    <pkt-alert v-if="dataLoadingError" skin="error">
+      {{ $t('integration.error.loading', { error: dataLoadingError }) }}
+    </pkt-alert>
+
+    <template v-else-if="apiClients.length">
+      <div>
+        <pkt-button skin="secondary" :disabled="loading" @onClick="create">
+          {{ $t('integration.action.add') }}
+        </pkt-button>
+      </div>
+
+      <api-client-card
+        v-for="client in apiClients"
+        :key="client.id"
+        :client="client"
+        :loading="loading"
+        :visible-secret="client.id === updatedClientId ? updatedClientSecret : null"
+        @hide-secret="hideSecret"
+        @edit="openClientModal"
+        @rotate="rotate"
+        @delete="archive"
+      />
+
+      <api-client-modal
+        v-if="showClientModal && chosenClient"
+        :client="chosenClient"
+        @save="update"
+        @close="closeClientModal"
+      />
+    </template>
+
+    <empty-state
+      v-else
+      :heading="$t('integration.empty.heading')"
+      :body="$t('integration.empty.body')"
+    >
+      <div data-mode="dark">
+        <pkt-button
+          skin="primary"
+          variant="icon-left"
+          icon-name="plus-sign"
+          @onClick="create"
+        >
+          {{ $t('integration.action.add') }}
+        </pkt-button>
+      </div>
+    </empty-state>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex';
+import { db } from '@/config/firebaseConfig';
+import { PktButton } from '@oslokommune/punkt-vue2';
+import ApiClient from '@/db/ApiClient';
+
+export default {
+  name: 'ItemAdminApiIntegrations',
+
+  components: {
+    PktAlert: () => import('@oslokommune/punkt-vue2').then(({ PktAlert }) => PktAlert),
+    EmptyState: () => import('@/components/EmptyState.vue'),
+    LoadingSmall: () => import('@/components/LoadingSmall.vue'),
+    ApiClientCard: () => import('@/components/ApiClientCard.vue'),
+    ApiClientModal: () => import('@/components/modals/ApiClientModal.vue'),
+    PktButton,
+  },
+
+  data: () => ({
+    apiClients: [],
+    dataLoading: false,
+    dataLoadingError: null,
+    loading: false,
+    showClientModal: false,
+    chosenClient: null,
+    updatedClientId: null,
+    updatedClientSecret: null,
+  }),
+
+  computed: {
+    ...mapState(['activeItemRef']),
+    ...mapGetters(['isAdmin', 'hasEditRights']),
+  },
+
+  watch: {
+    activeItemRef: {
+      immediate: true,
+      async handler(activeItemRef) {
+        this.dataLoading = true;
+        this.dataLoadingError = null;
+        await this.$bind(
+          'apiClients',
+          db
+            .collection('apiClients')
+            .where('parent', '==', activeItemRef)
+            .where('archived', '==', false)
+            .orderBy('created', 'desc')
+        ).catch((error) => {
+          this.dataLoadingError = error.code ? error.code : error;
+        });
+        this.dataLoading = false;
+      },
+    },
+  },
+
+  methods: {
+    async create() {
+      try {
+        this.loading = true;
+
+        const clientRef = await ApiClient.create({
+          parent: this.activeItemRef,
+          name: this.$t('integration.placeholderTitle'),
+        });
+
+        try {
+          const secret = await ApiClient.createSecret(clientRef);
+          this.updatedClientId = clientRef.id;
+          this.updatedClientSecret = secret;
+        } catch (error) {
+          await ApiClient.remove(clientRef);
+          throw error;
+        }
+
+        this.$toasted.show(this.$t('integration.toast.add'));
+      } catch (error) {
+        console.log(error);
+        this.$toasted.error(this.$t('integration.toast.addError'));
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    async update(client, data) {
+      try {
+        this.loading = true;
+        const clientRef = ApiClient.collection.doc(client.id);
+        await ApiClient.update(clientRef, data);
+        this.$toasted.show(this.$t('integration.toast.update'));
+      } catch (error) {
+        console.log(error);
+        this.$toasted.error(this.$t('integration.toast.updateError'));
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    async archive(client) {
+      try {
+        this.loading = true;
+        const clientRef = ApiClient.collection.doc(client.id);
+        await ApiClient.archive(clientRef);
+        this.$toasted.show(this.$t('integration.toast.delete'));
+      } catch (error) {
+        console.log(error);
+        this.$toasted.error(this.$t('integration.toast.deleteError'));
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    async rotate(client) {
+      try {
+        this.loading = true;
+        const clientRef = ApiClient.collection.doc(client.id);
+        const secret = await ApiClient.rotateSecret(clientRef);
+        this.updatedClientId = clientRef.id;
+        this.updatedClientSecret = secret;
+        this.$toasted.show(this.$t('integration.toast.rotate'));
+      } catch (error) {
+        console.log(error);
+        this.$toasted.error(this.$t('integration.toast.rotateError'));
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    hideSecret() {
+      this.updatedClientId = null;
+      this.updatedClientSecret = null;
+    },
+
+    openClientModal(client) {
+      this.showClientModal = true;
+      this.chosenClient = client;
+    },
+
+    closeClientModal() {
+      this.showClientModal = false;
+      this.chosenClient = null;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use '@oslokommune/punkt-css/dist/scss/abstracts/mixins/typography' as *;
+
+.integrations {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  padding-bottom: 3rem;
+}
+</style>
