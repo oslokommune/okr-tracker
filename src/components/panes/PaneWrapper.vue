90a34c904f43f8cfd18419b5d1e41c64b51a97c9
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2023-09-01 12:26:28 +0200

    Use absolute positioning for pane close button

--- src/components/panes/PaneWrapper.vue
+++ src/components/panes/PaneWrapper.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="pane">
+    <div class="pane__actions">
+      <slot name="actions" />
+      <pkt-button
+        v-if="closable"
+        v-tooltip="$t('btn.close')"
+        variant="icon-only"
+        skin="tertiary"
+        size="medium"
+        icon-name="close"
+        @onClick="$emit('close')"
+      />
+    </div>
+
     <div class="pane__inner">
       <div class="pane__header">
-        <h1 class="pkt-txt-20-medium">{{ title }}</h1>
-
-        <div class="pane__buttons">
-          <slot name="actions" />
-          <pkt-button
-            v-if="closable"
-            v-tooltip="$t('btn.close')"
-            variant="icon-only"
-            skin="tertiary"
-            size="small"
-            icon-name="close"
-            @onClick="$emit('close')"
-          />
-        </div>
+        <h1 class="pkt-txt-18-medium">{{ title }}</h1>
       </div>
 
       <div class="pane__body">
@@ -50,28 +50,27 @@ export default {
 
 <style lang="scss" scoped>
 .pane {
+  position: relative;
+
+  &__actions {
+    position: absolute;
+    top: 0.5rem;
+    right: 0.5rem;
+    display: flex;
+    gap: 0.5rem;
+  }
+
   &__inner {
     display: flex;
     flex-direction: column;
     gap: 1rem;
     height: 0;
-    padding: 1.5rem;
+    padding: 1.5rem 2.5rem;
   }
 
   &__header {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-  }
-
-  &__buttons {
-    display: flex;
-    gap: 0.5rem;
-  }
-
-  &__actions {
-    display: flex;
-    gap: 1rem;
     justify-content: space-between;
   }
 
