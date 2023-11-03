9193f36e93aca5f678128e99bf3e7edb980f2e43
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2023-11-03 09:14:24 +0100

    Make org admins able to add objective contributors

--- tests/firebase/utils.js
+++ tests/firebase/utils.js
@@ -15,11 +15,19 @@ export async function expectPermissionDenied(promise) {
   expect(errorResult.code).toBe('permission-denied' || 'PERMISSION_DENIED');
 }
 
+export async function expectAddSucceeds(promise) {
+  const successResult = await assertSucceeds(promise);
+  expect(successResult).not.toBeUndefined();
+  return successResult;
+}
+
 export async function expectUpdateSucceeds(promise) {
   const successResult = await assertSucceeds(promise);
   expect(successResult).toBeUndefined();
 }
 
 export async function expectGetSucceeds(promise) {
-  expect(assertSucceeds(promise)).not.toBeUndefined();
+  const successResult = await assertSucceeds(promise);
+  expect(successResult).not.toBeUndefined();
+  return successResult;
 }
