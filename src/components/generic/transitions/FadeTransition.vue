4517dbfbf03c578c8cd238dbb2a3af988a3b7b37
Petter Holt Juliussen <petter.juliussen@origo.oslo.kommune.no>
2024-11-08 15:05:52 +0100

    Add generic transition components

---
+++ src/components/generic/transitions/FadeTransition.vue
@@ -0,0 +1,18 @@
+<template>
+  <Transition>
+    <slot></slot>
+  </Transition>
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  transition: none;
+}
+</style>
